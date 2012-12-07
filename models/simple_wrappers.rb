74bda893eb7a8f9c8191ea219e0ff7278d9eeaf4
Paul R Alexander <palexander@stanford.edu>
2012-12-07 12:05:26 -0800

    Make sure ontologies_linked_data is available

--- models/simple_wrappers.rb
+++ models/simple_wrappers.rb
@@ -1,3 +1,5 @@
+require "ontologies_linked_data"
+
 # The classes defined here are simple wrappers around objects that exist in other namespaces
 # Wrapping them here allows access to them without using the full namespace.
 # If additional functionality is needed for ontologies_api only, the class should be moved to its own model file.
