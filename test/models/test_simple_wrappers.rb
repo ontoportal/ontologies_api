9f05213090c4ed019b858a0acc56a166d53fb058
Paul R Alexander <palexander@stanford.edu>
2012-12-07 12:01:36 -0800

    Tests for simple wrappers

---
+++ test/models/test_simple_wrappers.rb
@@ -0,0 +1,13 @@
+require "ontologies_linked_data"
+require_relative "../test_case"
+
+class TestSimpleWrappers < TestCase
+  def test_typing
+    assert Category.ancestors.include? LinkedData::Models::Category
+    assert Group.ancestors.include? LinkedData::Models::Group
+    assert Ontology.ancestors.include? LinkedData::Models::Ontology
+    assert Project .ancestors.include? LinkedData::Models::Project
+    assert Review.ancestors.include? LinkedData::Models::Review
+    assert User.ancestors.include? LinkedData::Models::User
+  end
+end
