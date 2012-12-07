91459dea5aea71e5f91682d2985344cc7e2a94be
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2012-12-07 14:53:59 -0800

    Add parens for some method calls, where eclipse-ruby IDE indicates warnings without the parens.

--- test/models/test_simple_wrappers.rb
+++ test/models/test_simple_wrappers.rb
@@ -3,11 +3,11 @@ require_relative "../test_case"
 
 class TestSimpleWrappers < TestCase
   def test_typing
-    assert Category.ancestors.include? LinkedData::Models::Category
-    assert Group.ancestors.include? LinkedData::Models::Group
-    assert Ontology.ancestors.include? LinkedData::Models::Ontology
-    assert Project .ancestors.include? LinkedData::Models::Project
-    assert Review.ancestors.include? LinkedData::Models::Review
-    assert User.ancestors.include? LinkedData::Models::User
+    assert Category.ancestors.include?(LinkedData::Models::Category)
+    assert Group.ancestors.include?(LinkedData::Models::Group)
+    assert Ontology.ancestors.include?(LinkedData::Models::Ontology)
+    assert Project .ancestors.include?(LinkedData::Models::Project)
+    assert Review.ancestors.include?(LinkedData::Models::Review)
+    assert User.ancestors.include?(LinkedData::Models::User)
   end
 end
