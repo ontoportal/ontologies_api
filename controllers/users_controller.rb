418d10052c635796daf2c5ee8337aeade4a00af2
Paul R Alexander <palexander@stanford.edu>
2012-12-11 11:08:35 -0800

    Users controller stub

---
+++ controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  namespace "/users" do
+    # Display all users
+    get do
+    end
+
+    # Display a single user
+    get '/:user' do
+    end
+
+    # Create a new user
+    post do
+    end
+
+    # Update via delete/create for an existing submission of an user
+    put '/:user' do
+    end
+
+    # Update an existing submission of an user
+    patch '/:user' do
+    end
+
+    # Delete a user
+    delete '/:user' do
+    end
+
+  end
+end
