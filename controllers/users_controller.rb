19c4e9b8d5f0db7d71e4737be6844a26d4e6ee58
Paul R Alexander <palexander@stanford.edu>
2012-12-12 12:19:06 -0800

    Users controller implementation

--- controllers/users_controller.rb
+++ controllers/users_controller.rb
@@ -1,28 +1,42 @@
-class UsersController < ApplicationController
 class UsersController
   namespace "/users" do
     # Display all users
     get do
+      s(User.all)
     end
 
     # Display a single user
-    get '/:user' do
+    get '/:username' do
+      s(User.find(params[:username]))
     end
 
-    # Create a new user
-    post do
-    end
-
-    # Update via delete/create for an existing submission of an user
-    put '/:user' do
+    # Users get created via put because clients can assign an id (POST is only used where servers assign ids)
+    put '/:username' do
+      user = instance_from_params(User, params)
+      if user.valid?
+        user.save
+      else
+        halt 400, {:errors => user.errors}
+      end
+      s(user, 201)
     end
 
     # Update an existing submission of an user
-    patch '/:user' do
+    patch '/:username' do
+      user = User.find([params[:username]])
+      populate_from_params(user, params)
+      if user.valid?
+        user.save
+      else
+        halt 400, {:errors => user.errors}
+      end
+      halt 204
     end
 
     # Delete a user
-    delete '/:user' do
+    delete '/:username' do
+      User.find(params[:username]).delete
+      halt 204
     end
 
   end
