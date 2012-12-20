f53b8ddcf23b2b4806f2c5cc8298febb27286000
Paul R Alexander <palexander@stanford.edu>
2012-12-20 14:27:16 -0800

    Test upload folder

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-# Config Goo connection for LinkedData (repo connection and namespaces)
-# LinkedData.config
+# This is the folder where uploaded ontology files are stored
+$REPOSITORY_FOLDER = File.expand_path('../../../test/data/uploaded_ontologies', __FILE__)
 
 require_relative "local/development.rb" if File.exist?("local/development.rb")
