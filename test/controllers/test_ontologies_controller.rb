691f7148311d41f4a79d5317f687cbfc03d495b7
Paul R Alexander <palexander@stanford.edu>
2012-12-20 14:29:46 -0800

    Test creating an ontology

--- test/controllers/test_ontologies_controller.rb
+++ test/controllers/test_ontologies_controller.rb
@@ -1,6 +1,39 @@
 require_relative '../test_case'
+require 'json'
 
 class TestOntologiesController < TestCase
+  def setup
+    _set_vars
+    _delete
+    _create_models
+  end
+
+  def teardown
+    _set_vars
+    _delete
+  end
+
+  def _set_vars
+    @acronym = "TST"
+    @name = "Test Ontology"
+  end
+
+  def _create_models
+    test_user = User.new(username: "tim")
+    test_user.save
+  end
+
+  def _delete
+    _delete_onts
+    test_user = User.find("tim")
+    test_user.delete unless test_user.nil?
+  end
+
+  def _delete_onts
+    ont = Ontology.find(@acronym)
+    ont.delete unless ont.nil?
+  end
+
   def test_all_ontologies
     get '/ontologies'
     assert last_response.ok?
@@ -17,7 +50,30 @@ class TestOntologiesController < TestCase
   def test_submissions_for_given_ontology
   end
 
-  def test_create_new_ontology
+  def test_create_new_ontology_same_acronym
+    ont = Ontology.new(acronym: @acronym, name: @name)
+    ont.save
+
+    put "/ontologies/#{@acronym}", :name => @name
+    assert last_response.status == 400
+  end
+
+  def test_create_new_ontology_invalid
+    put "/ontologies/#{@acronym}"
+    assert last_response.status == 400
+    assert JSON.parse(last_response.body)["errors"]
+  end
+
+  def test_create_new_ontology_missing_file_and_pull_location
+    put "/ontologies/#{@acronym}", name: @name
+    assert last_response.status == 400
+    assert JSON.parse(last_response.body)["errors"]
+  end
+
+  def test_create_new_ontology_file
+    test_file = File.expand_path("../../data/ontology_files/BRO_v3.1.owl", __FILE__)
+    put "/ontologies/#{@acronym}", name: @name, ontologyFormat: "OWL", administeredBy: "tim", "file" => Rack::Test::UploadedFile.new(test_file, "")
+    assert last_response.status == 201
   end
 
   def test_create_new_ontology_submission
