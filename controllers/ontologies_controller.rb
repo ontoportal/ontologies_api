47459baa5e27ee6c4b0d5e35417272543bb0d3e6
Paul R Alexander <palexander@stanford.edu>
2012-12-20 14:28:05 -0800

    Change :ontology to :acronym

--- controllers/ontologies_controller.rb
+++ controllers/ontologies_controller.rb
@@ -5,47 +5,45 @@ class OntologiesController
     end
 
     # Display the most recent submission of the ontology
-    get '/:ontology' do
+    get '/:acronym' do
       submission = params[:ontology_submission_id]
     end
 
     # Display all submissions of an ontology
-    get '/:ontology/submissions' do
+    get '/:acronym/submissions' do
       submission = params[:ontology_submission_id]
     end
 
     # Ontologies get created via put because clients can assign an id (POST is only used where servers assign ids)
-    put '/:ontology' do
-      filename, tmpfile = file_from_request
     end
 
     # Create a new submission for an existing ontology
-    post '/:ontology/submission' do
+    post '/:acronym/submission' do
     end
 
     # Update via delete/create for an existing submission of an ontology
-    put '/:ontology/:ontology_submission_id' do
+    put '/:acronym/:ontology_submission_id' do
     end
 
     # Update an existing submission of an ontology
-    patch '/:ontology/:ontology_submission_id' do
+    patch '/:acronym/:ontology_submission_id' do
     end
 
     # Delete an ontology and all its versions
-    delete '/:ontology' do
+    delete '/:acronym' do
     end
 
     # Delete a specific ontology submission
-    delete '/:ontology/:ontology_submission_id' do
+    delete '/:acronym/:ontology_submission_id' do
     end
 
     # Download an ontology
-    get '/:ontology/download' do
+    get '/:acronym/download' do
       submission = params[:ontology_submission_id]
     end
 
     # Properties for given ontology
-    get '/:ontology/properties' do
+    get '/:acronym/properties' do
     end
 
     private
