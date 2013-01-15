1830bc0805c5f1d3ae0d451cfee47c641ac6135e
Paul R Alexander <palexander@stanford.edu>
2013-01-14 17:11:26 -0800

    Method to create ontologies with submissions

--- test/test_case.rb
+++ test/test_case.rb
@@ -14,4 +14,60 @@ class TestCase < Test::Unit::TestCase
     Sinatra::Application
   end
 
+  def create_ontologies_and_submissions(options = {})
+    ont_count = options[:ont_count] || 5
+    submission_count = options[:submission_count] || 5
+    random_submission_count = options[:random_submission_count].nil? ? true : options[:random_submission_count]
+
+    u = LinkedData::Models::User.new(username: "tim")
+    u.save unless u.exist? || !u.valid?
+
+    of = LinkedData::Models::OntologyFormat.new(acronym: "OWL")
+    of.save unless u.exist? || !u.valid?
+
+    ont_acronyms = []
+    ont_count.to_i.times do |count|
+      acronym = "TST-ONT-#{count}"
+      ont_acronyms << acronym
+
+      o = LinkedData::Models::Ontology.new({
+        acronym: acronym,
+        name: "Test Ontology ##{count}",
+      })
+      o.save if o.valid?
+
+      # Random submissions (between 1 and max)
+      max = random_submission_count ? (1..submission_count.to_i).to_a.shuffle.first : submission_count
+      max.times do
+        os = LinkedData::Models::OntologySubmission.new({
+          acronym: "TST-ONT-#{count}",
+          ontology: o,
+          ontologyFormat: of,
+          administeredBy: u,
+          pullLocation: RDF::IRI.new("http://example.com"),
+          status: LinkedData::Models::SubmissionStatus.new(:code => "UPLOADED"),
+          submissionId: o.next_submission_id
+        })
+        os.save if os.valid?
+      end
+    end
+    return ont_count, ont_acronyms
+  end
+
+  def delete_ontologies_and_submissions
+    ont = LinkedData::Models::Ontology.find("TST-ONT-0")
+    count = 0
+    while ont
+      ont.delete
+      ont = LinkedData::Models::Ontology.find("TST-ONT-#{count+1}")
+    end
+
+    u = LinkedData::Models::User.find("tim")
+    u.delete unless u.nil?
+
+    of = LinkedData::Models::OntologyFormat.find("OWL")
+    of.delete unless u.nil?
+  end
+
 end
+
