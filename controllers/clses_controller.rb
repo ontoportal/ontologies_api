2594c61143eaacbbe518f64f25a5fc1e5d018b5d
msalvadores <msalvadores@gmail.com>
2013-01-29 19:09:50 -0800

    Get roots controlller and test.

--- controllers/clses_controller.rb
+++ controllers/clses_controller.rb
@@ -4,14 +4,37 @@ class ClsesController
     get do
     end
 
+    # Get root classes
+    get '/roots' do
+      ont = Ontology.find(params["ontology"])
+      error 400, "You must provide an existing `acronym` to retrieve roots" if ont.nil?
+      ont.load unless ont.loaded?
+      submission = nil
+      if params.include? :ontology_submission_id
+        submission = ont.submission(params[:ontology_submission_id])
+        error 400, "You must provide an existing submission ID for the #{params["acronym"]} ontology" if submission.nil?
+      else
+        submission = ont.latest_submission
+      end
+      submission.load unless submission.loaded?
+      status = submission.submissionStatus
+      if submission.nil?
+        error 400, "Ontology #{params["acronym"]} does not have any submissions" if submission.nil?
+      end
+      roots = submission.roots
+      roots.each do |r|
+        r.load_labels unless r.loaded_labels?
+      end
+      reply roots
+    end
+
+    #
     # Display a single class
     get '/:cls' do
       submission = params[:ontology_submission_id]
     end
 
-    # Get root classes
-    get '/roots' do
-    end
+
 
     # Get a tree view
     get '/:cls/tree' do
--- test/controllers/test_clses_controller.rb
+++ test/controllers/test_clses_controller.rb
@@ -17,6 +17,16 @@ class TestClsesController < TestCase
   end
 
   def test_roots_for_cls
+    num_onts_created, created_ont_acronyms = create_ontologies_and_submissions(ont_count: 1, submission_count: 1, process_submission: true)
+    ont = Ontology.find(created_ont_acronyms.first)
+    ont.load unless ont.loaded?
+    get "/ontologies/#{ont.acronym}/classes/roots"
+    assert last_response.ok?
+    roots = JSON.parse(last_response.body)
+    assert_equal 12, roots.length
+    roots.each do |r|
+      assert_instance_of String, r["prefLabel"]
+    end
   end
 
   def test_tree_for_cls
