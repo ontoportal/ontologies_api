9845f9e6f3006067109afca4ba59b651f810b30f
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2013-01-29 16:19:41 -0800

    Merge master branch - resolved conflicts, fixed goo-update failures on delete.

--- test/controllers/test_projects_controller.rb
+++ test/controllers/test_projects_controller.rb
@@ -3,6 +3,8 @@ require 'json-schema'
 
 class TestProjectsController < TestCase
 
+  DEBUG_MESSAGES=false
+
   # JSON Schema
   # This could be in the Project model, see
   # https://github.com/ncbo/ontologies_linked_data/issues/22
@@ -31,37 +33,37 @@ class TestProjectsController < TestCase
     JSON.parse(JSON_SCHEMA_STR)
   end
 
-  def _validate_json(data, list=false)
-    # Validate json data against a schema. Note that schema may be more
-    # restrictive than serializer.
-    schema = _project_json_schema
+  # Validate JSON object against a JSON schema.
+  # @note schema may be more restrictive than serializer generating json data.
+  # @param [Hash] jsonObj ruby hash created by JSON.parse
+  # @param [boolean] list set it true for jsonObj array of items to validate
+  def _validate_json(jsonObj, list=false)
+    jsonSchema = _project_json_schema
     assert(
-        JSON::Validator.validate(schema, data, :list => list),
-        JSON::Validator.fully_validate(schema, data, :validate_schema => true, :list => list).to_s
+        JSON::Validator.validate(jsonSchema, jsonObj, :list => list),
+        JSON::Validator.fully_validate(jsonSchema, jsonObj, :validate_schema => true, :list => list).to_s
     )
   end
 
-  def _valid_response_project(response, status)
-    assert_equal(status, response.status)
-    p = JSON.parse(response.body)
-    assert_instance_of(Hash, p)
-    assert_equal(@p.name, p['name'])
-    _validate_json(p)
-    return true
-  end
-
-  def _delete(modelList)
-    modelList.each do |x|
-      next if x.nil?
-      x.load
-      x.delete
+  # Clear the triple store models
+  # @param [Array] gooModelArray an array of GOO models
+  def _delete_models(gooModelArray)
+    gooModelArray.each do |m|
+      next if m.nil?
+      m.load
+      m.delete
     end
   end
 
+  # Clear the triple store models
   def teardown
-    _delete(LinkedData::Models::Project.all)
-    _delete(LinkedData::Models::Ontology.all)
-    _delete(LinkedData::Models::User.all)
+    _delete_models(LinkedData::Models::Project.all)
+    _delete_models(LinkedData::Models::Ontology.all)
+    _delete_models(LinkedData::Models::User.all)
+    @projectParams = nil
+    @user = nil
+    @ont = nil
+    @p = nil
   end
 
   def setup
@@ -91,7 +93,7 @@ class TestProjectsController < TestCase
 
   def test_all_projects
     get '/projects'
-    assert_equal(200, last_response.status)
+    _response_status(200, last_response)
     projects = JSON.parse(last_response.body)
     assert_instance_of(Array, projects)
     assert_equal(1, projects.length)
@@ -100,49 +102,81 @@ class TestProjectsController < TestCase
     _validate_json(p)
   end
 
-  def test_valid_project
-    get "/projects/#{@p.name}"
-    _valid_response_project(last_response, 200)
-  end
-
-  def test_invalid_project
-    get "/projects/missing_project"
-    assert_equal(404, last_response.status)
-  end
-
-  def test_new_project_success
-    # Ensure it doesn't exist first (undo the setup creation)
-    delete "/projects/#{@p.name}"
-    assert_equal(204, last_response.status)
+  def test_project_create_success
+    # Ensure it doesn't exist first (undo the setup @p.save creation)
+    _project_delete(@p.name)
     put "/projects/#{@p.name}", @projectParams.to_json, "CONTENT_TYPE" => "application/json"
-    _valid_response_project(last_response, 201)
-    test_valid_project
+    _response_status(201, last_response)
+    _project_get_success(@p.name, true)
   end
 
-  def test_new_project_failures
+  def test_project_create_conflict
     # Fail PUT for any project that already exists.
     put "/projects/#{@p.name}", @projectParams.to_json, "CONTENT_TYPE" => "application/json"
-    assert_equal(409, last_response.status)
+    _response_status(409, last_response)
+    # The existing project should remain valid
+    _project_get_success(@p.name, true)
+  end
+
+  def test_project_create_failure
     # Ensure the project doesn't exist.
-    delete "/projects/#{@p.name}"
-    assert_equal(204, last_response.status)
+    _project_delete(@p.name)
     # Fail PUT for any project with required missing data.
     @projectParams["name"] = nil
     put "/projects/#{@p.name}", @projectParams.to_json, "CONTENT_TYPE" => "application/json"
-    assert_equal(400, last_response.status)
+    _response_status(400, last_response)
+    _project_get_failure(@p.name)
   end
 
-  def test_update_project_success
+  def test_project_update_success
     patch "/projects/#{@p.name}", @projectParams.to_json, "CONTENT_TYPE" => "application/json"
-    assert_equal(204, last_response.status)
-    test_valid_project
+    _response_status(204, last_response)
+    _project_get_success(@p.name)
+    # TODO: validate the data updated
+    #_project_get_success(@p.name, true)
   end
 
-  def test_delete_project
-    delete "/projects/#{@p.name}"
-    assert_equal(204, last_response.status)
-    get "/projects/#{@p.name}"
-    assert_equal(404, last_response.status)
+  def test_project_delete
+    _project_delete(@p.name)
+    _project_get_failure(@p.name)
+  end
+
+
+  def _response_status(status, response)
+    if DEBUG_MESSAGES
+      assert_equal(status, response.status, response.body)
+    else
+      assert_equal(status, response.status)
+    end
+  end
+
+  # Issues DELETE for a project name, tests for a 204 response.
+  # @param [String] name project name
+  def _project_delete(name)
+    delete "/projects/#{name}"
+    _response_status(204, last_response)
+  end
+
+  # Issues GET for a project name, tests for a 200 response, with optional response validation.
+  # @param [String] name project name
+  # @param [boolean] validate_data verify response body json content
+  def _project_get_success(name, validate_data=false)
+    get "/projects/#{name}"
+    _response_status(200, last_response)
+    if validate_data
+      # Assume we have JSON data in the response body.
+      p = JSON.parse(last_response.body)
+      assert_instance_of(Hash, p)
+      assert_equal(@p.name, p['name'], p.to_s)
+      _validate_json(p)
+    end
+  end
+
+  # Issues GET for a project name, tests for a 404 response.
+  # @param [String] name project name
+  def _project_get_failure(name)
+    get "/projects/#{name}"
+    _response_status(404, last_response)
   end
 
 end
