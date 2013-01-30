5970362d158d0ee60aa8a0c8a8ccacef242cab1f
Michael Dorf <mdorf@stanford.edu>
2013-01-29 17:18:42 -0800

    implementing groups

--- controllers/groups_controller.rb
+++ controllers/groups_controller.rb
@@ -17,15 +17,45 @@ class GroupsController
 
     # Create a group with the given acronym
     put '/:group' do
+      acronym = params["group"]
+      group = Group.find(acronym)
+      if group.nil?
+        group = instance_from_params(Group, params)
+      else
+        error 400, "Group exists, please use HTTP PATCH to update"
+      end
+
+      if group.valid?
+        group.save
+      else
+        error 400, group.errors
+      end
+      reply 201, group
     end
 
     # Update an existing submission of an group
     patch '/:group' do
+      acronym = params["group"]
+      group = Group.find(acronym)
+      if group.nil?
+        error 400, "Group does not exist, please create using HTTP PUT before modifying"
+      else
+        populate_from_params(group, params)
+        if group.valid?
+          group.save
+        else
+          error 400, group.errors
+        end
+      end
+      halt 204
     end
 
     # Delete a group
     delete '/:group' do
+      g = Group.find(params["group"])
+      g.load unless g.loaded?
+      g.delete
+      halt 204
     end
-
   end
 end
--- test/controllers/test_groups_controller.rb
+++ test/controllers/test_groups_controller.rb
@@ -4,6 +4,8 @@ class TestgroupsController < TestCase
 
   def setup
     # Create some test groups
+    @test_group = {acronym: "TEST-GROUP", name: "Test Group", description: "Description of the Test Group"}
+
     @groups = {
       "CTSA-HOM" => ["CTSA Health Ontology Mapper Cancer", "CTSA Health Ontology Mapper Cancer Description"],
       "caBIG" => ["Biomedical Informatics Grid", "Biomedical Info Grid Description"],
@@ -32,6 +34,8 @@ class TestgroupsController < TestCase
   end
 
   def _delete_groups
+    test_group = Group.find(@test_group[:acronym])
+    test_group.delete unless test_group.nil?
     @groups.each do |acronym, name_desc|
       group = Group.find(acronym)
       group.delete unless group.nil?
@@ -55,13 +59,48 @@ class TestgroupsController < TestCase
   end
 
   def test_create_new_group
+    acronym = @test_group[:acronym]
+    put "/groups/#{acronym}", @test_group.to_json, "CONTENT_TYPE" => "application/json"
+
+    assert last_response.status == 201
+    assert JSON.parse(last_response.body)["acronym"].eql?(acronym)
+
+    get "/groups/#{acronym}"
+    assert last_response.ok?
+    assert JSON.parse(last_response.body)["acronym"].eql?(acronym)
   end
 
   def test_update_replace_group
   end
 
   def test_update_patch_group
+    acronym = 'CTSA-HOM'
+    group = Group.find(acronym)
+    assert_not_nil group
+    new_name = "CTSA Health Brand new NAME"
+    group.name = new_name
+
+    new_desc = "CTSA Health Brand new DESCRIPTION"
+    group.desc = new_desc
+
+
+
+    patch "/groups/#{acronym}", group.to_json, "CONTENT_TYPE" => "application/json"
+
+
+    binding.pry
+
+
+    assert last_response.status == 204
+
+
+
+    #get "/groups/#{acronym}"
+    #group = JSON.parse(last_response.body)
+    #assert group["name"].eql?(new_name)
+    #assert group["description"].eql?(new_desc)
   end
+
 
   def test_delete_group
   end
