dc6f0345988d033ccdd963ede2ac767b34ba894f
Paul R Alexander <palexander@stanford.edu>
2013-01-30 18:44:30 -0800

    Typo in TestCategoriesController declaration

--- test/controllers/test_categories_controller.rb
+++ test/controllers/test_categories_controller.rb
@@ -1,7 +1,7 @@
 require_relative '../test_case'
 require "json"
 
-class TestcategoriesController < TestCase
+class TestCategoriesController < TestCase
 
   def setup
     @category_ids = ["cat1", "cat2", "cat3"]
