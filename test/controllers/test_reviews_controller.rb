4b4a6496a1248126ba67dd9b6eb94549749d1e71
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2013-01-31 14:32:08 -0800

    Add json-schema to test review controller, after updating review model.

--- test/controllers/test_reviews_controller.rb
+++ test/controllers/test_reviews_controller.rb
@@ -1,6 +1,38 @@
 require_relative '../test_case'
 
 class TestReviewsController < TestCase
+
+  # JSON Schema
+  # This could be in the Review model, see
+  # https://github.com/ncbo/ontologies_linked_data/issues/22
+  # json-schema for description and validation of REST json responses.
+  # http://tools.ietf.org/id/draft-zyp-json-schema-03.html
+  # http://tools.ietf.org/html/draft-zyp-json-schema-03
+  JSON_SCHEMA_STR = <<-END_JSON_SCHEMA_STR
+  {
+    "type":"object",
+    "title":"Review",
+    "description":"A BioPortal ontology review.",
+    "additionalProperties":false,
+    "properties":{
+      "creator":{ "type":"string", "required": true },
+      "created":{ "type":"string", "format":"datetime", "required": true },
+      "body":{ "type":"string", "required": true },
+      "ontologyReviewed":{ "type":"string", "required": true },
+      "usabilityRating":{ "type":"number" },
+      "coverageRating":{ "type":"number" },
+      "qualityRating":{ "type":"number" },
+      "formalityRating":{ "type":"number" },
+      "correctnessRating":{ "type":"number" },
+      "documentationRating":{ "type":"number" },
+    }
+  }
+  END_JSON_SCHEMA_STR
+
+  def _project_json_schema
+    JSON.parse(JSON_SCHEMA_STR)
+  end
+
   def test_all_reviews
     get '/reviews'
     assert last_response.ok?
