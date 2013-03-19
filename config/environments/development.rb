526a5044b6fc0c7d44af3357d432f2ae185b9160
Paul R Alexander <palexander@stanford.edu>
2013-03-18 17:25:20 -0700

    Remove local configs

--- config/environments/development.rb
+++
@@ -1,6 +0,0 @@
-# This is the folder where uploaded ontology files are stored
-$REPOSITORY_FOLDER = File.expand_path('../../../test/data/uploaded_ontologies', __FILE__)
-
-
-local_path = File.expand_path("../local/development.rb", __FILE__)
-require local_path if File.exist?(local_path)
--- config/environments/local/README.md
+++
@@ -1,3 +0,0 @@
-# You can place local configuration details in this folder.
-# Names should match the top-level configuration files, IE:
-# development.rb, production.rb, etc
