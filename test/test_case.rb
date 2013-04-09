c6381cc9cc029f2f5425e156d83ef8034d325c49
Paul R Alexander <palexander@stanford.edu>
2013-04-08 17:09:45 -0700

    Can't set these in Sinatra 1.4

--- test/test_case.rb
+++ test/test_case.rb
@@ -39,9 +39,6 @@ class TestCase < Test::Unit::TestCase
 
   def app
     Sinatra::Application
-    set :raise_errors, true
-    set :dump_errors, false
-    set :show_exceptions, false
   end
 
   def teardown
