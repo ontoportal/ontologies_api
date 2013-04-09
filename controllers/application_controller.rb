876ce3fd4b5b350332ae0f335e00280046f3c53f
Paul R Alexander <palexander@stanford.edu>
2013-04-08 17:09:16 -0700

    Make ApplicationController work in Sinatra 1.4

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -1,6 +1,9 @@
 # This is the base class for controllers in the application.
 # Code in the before or after blocks will run on every request
 class ApplicationController
+  include Sinatra::Delegator
+  extend Sinatra::Delegator
+
   before {
     # Run before route
   }
