323e9e9a15e4d2f73c2cb6f0f145742dd249e07d
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2013-04-16 16:00:41 -0700

    Modified RI wrapper to add JSON schema validations and ranked_elements endpoint.

--- controllers/resource_index_controller.rb
+++ controllers/resource_index_controller.rb
@@ -1,16 +1,15 @@
 
 require 'ncbo_resource_index'
 
-
 class ResourceIndexController < ApplicationController
 
-  # Note get_options method is in resource_index_helper.rb
+  # Note: methods from resource_index_helper.rb :
+  #  -  get_options(params)
+  #  -  get_ontology_virtual_id(ontology_acronym)
 
   namespace "/resource_index" do
 
-    # Return search results
-    get '/search' do
-      options = get_options(params)
+    def set_classes(params)
       # Assume request signature of the form:
       # classes[acronym1][classid1,classid2,classid3]&classes[acronym2][classid1,classid2]
       if params.key?("classes")
@@ -20,47 +19,114 @@ class ResourceIndexController < ApplicationController
           # Use 'k' as an ontology acronym, translate it to an ontology virtual ID.
           ont_id = get_ontology_virtual_id(k)
           next if ont_id == nil
-          # TODO: use 'v' as a CSV list of concepts (class ID)
+          # Use 'v' as a CSV list of concepts (class ID)
           v.split(',').each do |class_id|
             classes.push("#{ont_id}/#{class_id}")
           end
         end
+        return classes
+      else
+        #
+        # TODO: reply with syntax error message?
+        #
+      end
+    end
+
+    # Return search results
+    get '/search' do
+      options = get_options(params)
+      # Assume request signature of the form:
+      # classes[acronym1][classid1,classid2,classid3]&classes[acronym2][classid1,classid2]
+      classes = set_classes(params)
+      if classes.empty?
+        #
+        # TODO: reply with syntax error message?
+        #
+      else
         result = NCBO::ResourceIndex.find_by_concept(classes, options)
-        #binding.pry
         reply result
       end
-      #
-      # TODO: reply with syntax error message?
-      #
+    end
+
+    # Return ranked elements
+    get '/ranked_elements' do
+      options = get_options(params)
+      classes = set_classes(params)
+      if classes.empty?
+        #
+        # TODO: reply with syntax error message?
+        #
+      else
+        result = NCBO::ResourceIndex.ranked_elements(classes, options)
+        result.resources.each do |r|
+          r[:elements] = elementsMassage(r[:elements])
+        end
+        reply result
+      end
     end
 
     # Return all resources
     get "/resources" do
       options = get_options(params)
-      response = NCBO::ResourceIndex.resources(options)
-      # TODO: massage the return values (may need 'models' for elements, annotations etc.)
-      reply response
+      result = NCBO::ResourceIndex.resources(options)
+      reply resourceMassage(result)
     end
 
     # Return a specific resource
     get "/resources/:resource_id" do
       options = get_options(params)
-      response = NCBO::ResourceIndex.resources(options)
-      # TODO: massage the return values (may need 'models' for elements, annotations etc.)
-      reply response
+      result = NCBO::ResourceIndex.resources(options)
+      reply resourceMassage(result)
     end
 
     # Return a specific element from a specific resource
     get "/resources/:resource_id/elements/:element_id" do
       options = get_options(params)
-      response = NCBO::ResourceIndex.resources(options)
-      # TODO: massage the return values (may need 'models' for elements, annotations etc.)
-      reply response
+      result = NCBO::ResourceIndex.resources(options)
+      # TODO: Use the element method instead (Paul is fixing bug)
+      #result = NCBO::ResourceIndex.element(params["element_id"], params["resource_id"], options)
+      #binding.pry
+      reply resourceMassage(result)
     end
 
     #
     # TODO: enable POST methods?
     #
-  end
 
-end
+    def elementsMassage(elementArray)
+      elements = []
+      elementArray.each do |e|
+        element = {
+            "id" => e[:localElementId],
+            "fields" => []
+        }
+        e[:text].each do |name, description|
+          weight = 0.0
+          e[:weights].each {|hsh| weight = hsh[:weight] if hsh[:name] == name}
+          ontID = [e[:ontoIds][name]].flatten  # Wrap Fixnum or Array into Array
+          element["fields"].push(
+            {
+              "name" => name,
+              "text" => description,
+              "weight" => weight,
+              "associatedOntologies" => ontID
+            }
+          )
+        end
+        elements.push element
+      end
+      return elements
+    end
+
+    def resourceMassage(resourceArray)
+      # Remove resource content
+      resourceArray.each do |r|
+        r.delete :workflowCompletedDate
+        r.delete :contexts
+      end
+      return resourceArray
+    end
+
+  end # namespace "/resource_index"
+
+end # class ResourceIndexController
--- test/controllers/test_resource_index_controller.rb
+++ test/controllers/test_resource_index_controller.rb
@@ -6,31 +6,119 @@ class TestResourceIndexController < TestCase
   DEBUG_MESSAGES=false
 
   # JSON Schema
-  # This could be in the Project model, see
-  # https://github.com/ncbo/ontologies_linked_data/issues/22
   # json-schema for description and validation of REST json responses.
   # http://tools.ietf.org/id/draft-zyp-json-schema-03.html
   # http://tools.ietf.org/html/draft-zyp-json-schema-03
-  #JSON_SCHEMA_STR = <<-END_JSON_SCHEMA_STR
-  #{
-  #  "type":"object",
-  #  "title":"Project",
-  #  "description":"A BioPortal project, which may refer to multiple ontologies.",
-  #  "additionalProperties":true,
-  #  "properties":{
-  #    "@id":{ "type":"string", "format":"uri", "required": true },
-  #    "@type":{ "type":"string", "format":"uri", "required": true },
-  #    "acronym":{ "type":"string", "required": true },
-  #    "name":{ "type":"string", "required": true },
-  #    "creator":{ "type":"string", "required": true },
-  #    "created":{ "type":"string", "format":"datetime", "required": true },
-  #    "homePage":{ "type":"string", "format":"uri", "required": true },
-  #    "description":{ "type":"string", "required": true },
-  #    "institution":{ "type":"string" },
-  #    "ontologyUsed":{ "type":"array", "items":{ "type":"string" } }
-  #  }
-  #}
-  #END_JSON_SCHEMA_STR
+  RESOURCE_SCHEMA_STR = <<-END_RESOURCE_SCHEMA_STR
+  {
+    "type": "object",
+    "title": "resource",
+    "description": "A Resource Index resource.",
+    "additionalProperties": false,
+    "properties": {
+      "resourceName": {
+        "type": "string",
+        "required": true
+      },
+      "resourceId": {
+        "type": "string",
+        "required": true
+      },
+      "mainContext": {
+        "type": "string",
+        "required": true
+      },
+      "resourceURL": {
+        "type": "string",
+        "format": "uri",
+        "required": true
+      },
+      "resourceElementURL": {
+        "type": "string",
+        "format": "uri"
+      },
+      "resourceDescription": {
+        "type": "string"
+      },
+      "resourceLogo": {
+        "type": "string",
+        "format": "uri"
+      },
+      "lastUpdateDate": {
+        "type": "string",
+        "format": "datetime"
+      },
+      "totalElements": {
+        "type": "number"
+      }
+    }
+  }
+  END_RESOURCE_SCHEMA_STR
+  RESOURCES_SCHEMA_STR = <<-END_RESOURCES_SCHEMA_STR
+  {
+    "type": "array",
+    "title": "resources",
+    "description": "An array of Resource Index resource objects.",
+    "items": {
+      "type": "object"
+    }
+  }
+  END_RESOURCES_SCHEMA_STR
+
+  ELEMENT_FIELD_SCHEMA_STR = <<-END_ELEMENT_FIELD_SCHEMA_STR
+  {
+    "type": "object",
+    "title": "element_field",
+    "description": "A Resource Index element field.",
+    "additionalProperties": false,
+    "properties": {
+      "name": {
+        "type": "string",
+        "required": true
+      },
+      "text": {
+        "type": "string",
+        "required": true
+      },
+      "weight": {
+        "type": "number",
+        "required": true
+      },
+      "associatedOntologies": {
+        "type": "array",
+        "required": true
+      }
+    }
+  }
+  END_ELEMENT_FIELD_SCHEMA_STR
+  ELEMENT_SCHEMA_STR = <<-END_ELEMENT_SCHEMA_STR
+  {
+    "type": "object",
+    "title": "element",
+    "description": "A Resource Index element.",
+    "additionalProperties": false,
+    "properties": {
+      "id": {
+        "type": "string",
+        "required": true
+      },
+      "fields": {
+        "type": "array",
+        "required": true
+      }
+    }
+  }
+  END_ELEMENT_SCHEMA_STR
+  ELEMENTS_SCHEMA_STR = <<-END_ELEMENTS_SCHEMA_STR
+  {
+    "type": "array",
+    "title": "elements",
+    "description": "An array of Resource Index element objects.",
+    "items": {
+      "type": "object"
+    }
+  }
+  END_ELEMENTS_SCHEMA_STR
 
 
   def teardown
@@ -59,34 +147,60 @@ class TestResourceIndexController < TestCase
     #}
   end
 
-  def test_all_resources
-    get '/resource_index/resources'
+  def test_get_ranked_elements
+    #get "/resource_index/search?classes[acronym1][classid1,classid2,classid3]&classes[acronym2][classid1,classid2]"
+    acronym = 'nif'
+    classid1 = 'obo2:DOID_1909' # Melanoma
+    classid2 = 'activity:IRB'
+    get "/resource_index/ranked_elements?classes[#{acronym}]=#{classid1}"
     _response_status(200, last_response)
-    #projects = MultiJson.load(last_response.body)
-    #assert_instance_of(Array, projects)
-    #assert_equal(1, projects.length)
-    #p = projects[0]
-    #assert_equal(@p.name, p['name'])
-    #validate_json(last_response.body, JSON_SCHEMA_STR, true)
+    #validate_json(last_response.body, RESOURCE_SCHEMA_STR, true)
+    results = MultiJson.load(last_response.body)
+    results["resources"].each do |r|
+      elements = r["elements"]
+      validate_json(MultiJson.dump(elements), ELEMENT_SCHEMA_STR, true)
+      elements.each do |e|
+        fields = e["fields"]
+        validate_json(MultiJson.dump(fields), ELEMENT_FIELD_SCHEMA_STR, true)
+      end
+    end
+    #binding.pry
+    #assert_instance_of(Array, elements)
+    # TODO: Check for an empty Array?
   end
 
-  def test_get_classes
+  def test_get_search_classes
     #get "/resource_index/search?classes[acronym1][classid1,classid2,classid3]&classes[acronym2][classid1,classid2]"
     #resource_id = 'GEO'
-    acronym = 'NCIt'
-    classid1 = 'Melanoma'
+    acronym = 'nif'
+    classid1 = 'obo2:DOID_1909' # Melanoma
     classid2 = 'activity:IRB'
     get "/resource_index/search?classes[#{acronym}]=#{classid1}"
-    _response_status(200, last_response)
     #get "/resource_index/search?classes[#{acronym}]=#{classid1},#{classid2}"
-    #_response_status(200, last_response)
+    _response_status(200, last_response)
+    validate_json(last_response.body, RESOURCE_SCHEMA_STR, true)
+    resources = MultiJson.load(last_response.body)
+    assert_instance_of(Array, resources)
+    not assert_empty(resources, "Error: empty resources.")
+    # TODO: Check for an empty Array?
   end
 
-  def test_get_element_resources
+  def test_get_resources
+    get '/resource_index/resources'
+    _response_status(200, last_response)
+    validate_json(last_response.body, RESOURCE_SCHEMA_STR, true)
+    resources = MultiJson.load(last_response.body)
+    assert_instance_of(Array, resources)
+  end
+
+  def test_get_resource_element
     resource_id = 'GEO'
     element_id = 'E-GEOD-19229'
     get "/resource_index/resources/#{resource_id}/elements/#{element_id}"
     _response_status(200, last_response)
+    validate_json(last_response.body, RESOURCE_SCHEMA_STR, true)
+    resources = MultiJson.load(last_response.body)
+    assert_instance_of(Array, resources)
   end
 
   def _response_status(status, response)
