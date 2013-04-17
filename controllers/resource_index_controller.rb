04f11c25bf5ecfb2ec3b466fdd4411f3cd4cfe3d
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2013-04-17 13:25:48 -0700

    Refactor helper methods; add more options parsing.

--- controllers/resource_index_controller.rb
+++ controllers/resource_index_controller.rb
@@ -9,49 +9,23 @@ class ResourceIndexController < ApplicationController
 
   namespace "/resource_index" do
 
-    def set_classes(params)
-      # Assume request signature of the form:
-      # classes[acronym1][classid1,classid2,classid3]&classes[acronym2][classid1,classid2]
-      if params.key?("classes")
-        classes = []
-        class_hash = params["classes"]
-        class_hash.each do |k,v|
-          # Use 'k' as an ontology acronym, translate it to an ontology virtual ID.
-          ont_id = get_ontology_virtual_id(k)
-          next if ont_id == nil
-          # Use 'v' as a CSV list of concepts (class ID)
-          v.split(',').each do |class_id|
-            classes.push("#{ont_id}/#{class_id}")
-          end
-        end
-        return classes
-      else
-        #
-        # TODO: reply with syntax error message?
-        #
-      end
-    end
-
-    # Return search results
     get '/search' do
       options = get_options(params)
-      # Assume request signature of the form:
-      # classes[acronym1][classid1,classid2,classid3]&classes[acronym2][classid1,classid2]
-      classes = set_classes(params)
+      classes = get_classes(params)
       if classes.empty?
         #
         # TODO: reply with syntax error message?
         #
       else
         result = NCBO::ResourceIndex.find_by_concept(classes, options)
+        # TODO: massage the result format
         reply result
       end
     end
 
-    # Return ranked elements
     get '/ranked_elements' do
       options = get_options(params)
-      classes = set_classes(params)
+      classes = get_classes(params)
       if classes.empty?
         #
         # TODO: reply with syntax error message?
@@ -59,7 +33,7 @@ class ResourceIndexController < ApplicationController
       else
         result = NCBO::ResourceIndex.ranked_elements(classes, options)
         result.resources.each do |r|
-          r[:elements] = elementsMassage(r[:elements])
+          r[:elements] = massage_elements(r[:elements])
         end
         reply result
       end
@@ -69,14 +43,14 @@ class ResourceIndexController < ApplicationController
     get "/resources" do
       options = get_options(params)
       result = NCBO::ResourceIndex.resources(options)
-      reply resourceMassage(result)
+      reply massage_resources(result)
     end
 
     # Return a specific resource
     get "/resources/:resource_id" do
       options = get_options(params)
       result = NCBO::ResourceIndex.resources(options)
-      reply resourceMassage(result)
+      reply massage_resources(result)
     end
 
     # Return a specific element from a specific resource
@@ -86,16 +60,16 @@ class ResourceIndexController < ApplicationController
       # TODO: Use the element method instead (Paul is fixing bug)
       #result = NCBO::ResourceIndex.element(params["element_id"], params["resource_id"], options)
       #binding.pry
-      reply resourceMassage(result)
+      reply massage_resources(result)
     end
 
     #
     # TODO: enable POST methods?
     #
 
-    def elementsMassage(elementArray)
+    def massage_elements(element_array)
       elements = []
-      elementArray.each do |e|
+      element_array.each do |e|
         element = {
             "id" => e[:localElementId],
             "fields" => []
@@ -118,13 +92,13 @@ class ResourceIndexController < ApplicationController
       return elements
     end
 
-    def resourceMassage(resourceArray)
+    def massage_resources(resource_array)
       # Remove resource content
-      resourceArray.each do |r|
+      resource_array.each do |r|
         r.delete :workflowCompletedDate
         r.delete :contexts
       end
-      return resourceArray
+      return resource_array
     end
 
   end # namespace "/resource_index"
