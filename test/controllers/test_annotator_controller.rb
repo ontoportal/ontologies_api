30fa5eb2a9d67816b55bb75ad422ae18c5aa746d
msalvadores <msalvadores@gmail.com>
2013-04-24 18:42:06 -0700

    confussing comment out

--- test/controllers/test_annotator_controller.rb
+++ test/controllers/test_annotator_controller.rb
@@ -26,13 +26,6 @@ eos
     assert last_response.ok?
     annotations = MultiJson.load(last_response.body)
     #paul: here you can reproduce the serializer output
-    #this is what I get:
-    # {"cls"=>
-    # "http://bioontology.org/ontologies/BiomedicalResourceOntology.owl#Aggregate_Human_Data",
-    # "hierarchy"=>
-    #  [{"cls"=>"#<LinkedData::Models::Class:0x007fce65bb9708>", "distance"=>1},
-    #   {"cls"=>"#<LinkedData::Models::Class:0x007fce65bfaed8>", "distance"=>2},
-    # (..)
 
     #TODO
     #this test is a replication of what is tested in linked data
