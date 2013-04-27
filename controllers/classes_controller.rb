ae894a2aded0af1479777fadc7a7df1065219827
Paul R Alexander <palexander@stanford.edu>
2013-04-26 18:23:11 -0700

    Typo fix descendents

--- controllers/classes_controller.rb
+++ controllers/classes_controller.rb
@@ -67,8 +67,8 @@ class ClassesController < ApplicationController
       reply ancestors
     end
 
-    # Get all descendants for given class
-    get '/:cls/descendants' do
+    # Get all descendents for given class
+    get '/:cls/descendents' do
       ont, submission = get_ontology_and_submission
       page, size = page_params
       ld = LinkedData::Models::Class.goo_attrs_to_load(includes_param)
