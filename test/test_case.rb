45b12a8dfb0ebbc27febbdb26f61eccea5c68c6d
Paul R Alexander <palexander@stanford.edu>
2013-04-26 19:25:25 -0700

    Warn about tests against potential production machines

--- test/test_case.rb
+++ test/test_case.rb
@@ -20,9 +20,15 @@ require 'json-schema'
 ENV['RACK_ENV'] = 'test'
 
 # Check to make sure you want to run if not pointed at localhost
-if !LinkedData.settings.goo_host.eql?("localhost")
-  print "\n\n================================== WARNING =================================="
-  print "\nYou are using a triplestore at #{LinkedData.settings.goo_host}. Tests can be destructive.\nType 'y' to continue: "
+safe_host = Regexp.new(/localhost|ncbo-dev*/)
+unless LinkedData.settings.goo_host.match(safe_host) && LinkedData.settings.search_server_url.match(safe_host) && LinkedData.settings.redis_host.match(safe_host)
+  print "\n\n================================== WARNING ==================================\n"
+  print "** TESTS CAN BE DESTRUCTIVE -- YOU ARE POINTING TO A POTENTIAL PRODUCTION/STAGE SERVER **\n"
+  print "Servers:\n"
+  print "triplestore -- #{LinkedData.settings.goo_host}\n"
+  print "search -- #{LinkedData.settings.search_server_url}\n"
+  print "redis -- #{LinkedData.settings.redis_host}\n"
+  print "Type 'y' to continue: "
   $stdout.flush
   confirm = $stdin.gets
   if !(confirm.strip == 'y')
