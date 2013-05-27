a43a10e72f70760b18dadcd106cd41f2e523fd9e
Paul R Alexander <palexander@stanford.edu>
2013-05-26 19:49:53 -0700

    Don't delete onts

--- test/test_case.rb
+++ test/test_case.rb
@@ -45,7 +45,6 @@ class AppUnit < MiniTest::Unit
 
   def after_suites
     # code to run after the last test (gets inherited in sub-tests)
-    LinkedData::SampleData::Ontology.delete_ontologies_and_submissions
   end
 
   def _run_suites(suites, type)
