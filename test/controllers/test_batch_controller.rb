89a09effe29558b26054f1686fc846080c4f0a74
msalvadores <msalvadores@gmail.com>
2013-05-29 18:30:11 -0700

    batch controller and tests

---
+++ test/controllers/test_batch_controller.rb
@@ -0,0 +1,96 @@
+require_relative '../test_case'
+
+class TestBatchController < TestCase
+  def self.before_suite 
+    LinkedData::SampleData::Ontology.delete_ontologies_and_submissions
+    @@ontologies = LinkedData::SampleData::Ontology.sample_owl_ontologies
+  end
+
+  def self.after_suite
+    LinkedData::SampleData::Ontology.delete_ontologies_and_submissions
+  end
+
+  def test_class_batch_one_ontology
+    ont_id = @@ontologies.map { |x| x.id.to_s }.select { |y| y.include? "BRO"}.first
+    assert ont_id, "BRO is not found to exectute batch test."
+    class_ids = {
+      "http://bioontology.org/ontologies/BiomedicalResourceOntology.owl#Information_Resource" => "Information Resource",
+      "http://bioontology.org/ontologies/BiomedicalResourceOntology.owl#Data_Resource" => "Data Resource",
+      "http://bioontology.org/ontologies/BiomedicalResourceOntology.owl#Clinical_Care_Data" => "Clinical Care Data",
+      "http://bioontology.org/ontologies/BiomedicalResourceOntology.owl#Aggregate_Human_Data" => "Aggregate Human Data"
+    }
+    collection = class_ids.keys.map { |x| { "class" => x , "ontology" => ont_id } }
+    call_params = {
+      "http://www.w3.org/2002/07/owl#Class" => {
+        "collection" => collection,
+        "include" => ["prefLabel","synonym"]
+    } }
+    post "/batch/", call_params
+    assert last_response.ok?
+    data = MultiJson.load(last_response.body)
+    classes = data["http://www.w3.org/2002/07/owl#Class"]
+    assert classes.length == 4
+    classes.each do |klass|
+      assert_instance_of String, klass["prefLabel"]
+      assert_instance_of Array, klass["synonym"]
+      assert klass["prefLabel"] == class_ids[klass["@id"]]
+    end
+  end
+
+  def test_class_batch_multiple
+    bro = @@ontologies.map { |x| x.id.to_s }.select { |y| y.include? "BRO"}.first
+    mccl = @@ontologies.map { |x| x.id.to_s }.select { |y| y.include? "MCCL"}.first
+    assert bro, "BRO is not found to exectute batch test."
+    assert mccl, "mccl is not found to exectute batch test."
+    class_ids = {
+      "http://bioontology.org/ontologies/BiomedicalResourceOntology.owl#Information_Resource" => "Information Resource",
+      "http://bioontology.org/ontologies/BiomedicalResourceOntology.owl#Data_Resource" => "Data Resource",
+      "http://bioontology.org/ontologies/BiomedicalResourceOntology.owl#Clinical_Care_Data" => "Clinical Care Data",
+      "http://bioontology.org/ontologies/BiomedicalResourceOntology.owl#Aggregate_Human_Data" => "Aggregate Human Data",
+      "http://purl.obolibrary.org/obo/MCBCC_0000288#ChromosomalMutation" => "chromosomal mutation",
+      "http://purl.obolibrary.org/obo/MCBCC_0000275#ReceptorAntagonists" => "receptor antagonists"
+    }
+    collection = class_ids.keys.map { |x| { "class" => x , "ontology" => (x.include?("obolibrary") ? mccl : bro) } }
+    call_params = {
+      "http://www.w3.org/2002/07/owl#Class" => {
+        "collection" => collection,
+        "include" => ["prefLabel"]
+    } }
+    post "/batch/", call_params
+    assert last_response.ok?
+    data = MultiJson.load(last_response.body)
+    classes = data["http://www.w3.org/2002/07/owl#Class"]
+    assert classes.length == 6
+    classes.each do |klass|
+      assert_instance_of String, klass["prefLabel"]
+      assert !klass["synonym"]
+      assert klass["prefLabel"] == class_ids[klass["@id"]]
+    end
+  end
+
+
+  def test_class_all_bro
+    mccl = @@ontologies.select { |y| y.id.to_s.include? "MCCL"}.first
+    assert mccl, "mccl is not found to exectute batch test."
+    classes = LinkedData::Models::Class.in(mccl.latest_submission).include(:prefLabel).page(1,500).read_only.all
+    class_ids = {}
+    classes.each do |klass|
+      class_ids[klass.id.to_s]=klass.prefLabel
+    end
+    collection = class_ids.keys.map { |x| { "class" => x , "ontology" => mccl.id.to_s } }
+    call_params = {
+      "http://www.w3.org/2002/07/owl#Class" => {
+        "collection" => collection,
+        "include" => ["prefLabel"]
+    } }
+    post "/batch/", call_params
+    assert last_response.ok?
+    data = MultiJson.load(last_response.body)
+    classes_response = data["http://www.w3.org/2002/07/owl#Class"]
+    assert classes_response.length = classes.length
+    classes_response.each do |klass|
+      assert_instance_of String, klass["prefLabel"]
+      assert klass["prefLabel"] == class_ids[klass["@id"]]
+    end
+  end
+end
