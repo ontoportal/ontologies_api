729116564a5a8d9f09ea235c169880742256bef1
Paul R Alexander <palexander@stanford.edu>
2013-06-03 16:39:05 -0700

    Make ont ids conform to triplestore, throw 404 when ont isn't found

--- controllers/batch_controller.rb
+++ controllers/batch_controller.rb
@@ -1,17 +1,15 @@
 class BatchController < ApplicationController
-
-
   namespace "/batch" do
     post do "/"
-      if params.keys != ["http://www.w3.org/2002/07/owl#Class"]
-        reply 422, "Batch endpoint only support calls to owl:Class resources"
+      unless params.key?("http://www.w3.org/2002/07/owl#Class")
+        error 422, "Batch endpoint only support calls to owl:Class resources"
       end
       resource_type = params.keys.first
       batch_params = params[resource_type]
       incl = batch_params["include"]
       collection = batch_params["collection"]
-      reply 422, "Call should contain 'include' parameters" if incl.nil?
-      reply 422, "Call should contain 'collection' parameter" if collection.nil? || collection.length == 0
+      error 422, "Call should contain 'include' parameters" if incl.nil?
+      error 422, "Call should contain 'collection' parameter" if collection.nil? || collection.length == 0
       goo_include = LinkedData::Models::Class.goo_attrs_to_load(incl)
       class_id_by_ontology = {}
       collection.each do |class_input|
@@ -20,10 +18,12 @@ class BatchController < ApplicationController
       end
       classes = []
       class_id_by_ontology.each do |ont_id,class_ids|
+        ont_id = ont_id.sub(LinkedData.settings.rest_url_prefix, Goo.id_prefix)
         class_ids.uniq!
         class_ids.map! { |id| RDF::URI.new(id) }
         ont = LinkedData::Models::Ontology.find(RDF::URI.new(ont_id))
                     .include(submissions: [:submissionId]).first
+        error 404, "Ontology #{ont_id} could not be found" if ont.nil?
         latest = ont.latest_submission
         latest.bring(ontology:[:acronym])
         classes.concat(LinkedData::Models::Class.in(latest).ids(class_ids).include(goo_include.map{|x| x.to_sym}).read_only.all)
