34d6631bf1ab87d7424a67b2111a19be13437e1e
Paul R Alexander <palexander@stanford.edu>
2013-06-12 00:06:07 -0700

    Typo fix

--- test/models/test_simple_wrappers.rb
+++ test/models/test_simple_wrappers.rb
@@ -6,7 +6,7 @@ class TestSimpleWrappers < TestCase
     assert Category.ancestors.include?(LinkedData::Models::Category)
     assert Group.ancestors.include?(LinkedData::Models::Group)
     assert Ontology.ancestors.include?(LinkedData::Models::Ontology)
-    assert Project .ancestors.include?(LinkedData::Models::Project)
+    assert Project.ancestors.include?(LinkedData::Models::Project)
     assert Review.ancestors.include?(LinkedData::Models::Review)
     assert User.ancestors.include?(LinkedData::Models::User)
   end
