e14ba36e57b601a967f7fa8359a7d7dabd1ebbd5
Paul R Alexander <palexander@stanford.edu>
2013-06-24 22:06:14 -0700

    Notes controller tests

--- test/controllers/test_notes_controller.rb
+++ test/controllers/test_notes_controller.rb
@@ -1,9 +1,13 @@
-require 'json'
+require 'cgi'
 require_relative '../test_case'
 
 class TestNotesController < TestCase
 
   def self.before_suite
+    self.new("before_suite").delete_ontologies_and_submissions
+    @@ontology, cls = self.new("before_suite")._ontology_and_class
+    @@cls = LinkedData::Models::Class.find(cls.id).in(@@ontology.latest_submission).include(:notes).first
+
     @@note_user = "test_note_user"
     _delete_user
     @@user = LinkedData::Models::User.new(
@@ -18,7 +22,9 @@ class TestNotesController < TestCase
       note = LinkedData::Models::Note.new({
         creator: @@user,
         subject: "Test subject #{i}",
-        body: "Test body for note #{i}"
+        body: "Test body for note #{i}",
+        relatedOntology: [@@ontology],
+        relatedClass: [@@cls]
       })
       note.save
       @@notes << note
@@ -26,9 +32,17 @@ class TestNotesController < TestCase
   end
 
   def self.after_suite
+    self.new("after_suite").delete_ontologies_and_submissions
     notes = LinkedData::Models::Note.where(creator: @@user)
     notes.each {|n| n.delete}
     _delete_user
+  end
+
+  def _ontology_and_class
+    count, acronyms, ontologies = create_ontologies_and_submissions(ont_count: 1, submission_count: 1, process_submission: true)
+    ontology = ontologies.first
+    cls = LinkedData::Models::Class.where.include(:prefLabel).in(ontology.latest_submission).read_only.page(1, 1).first
+    return ontology, cls
   end
 
   def self._delete_user
@@ -59,7 +73,7 @@ class TestNotesController < TestCase
       body: "Testing body",
       subject: "Testing subject"
     }
-    post "/notes",MultiJson.dump(note), "CONTENT_TYPE" => "application/json"
+    post "/notes", MultiJson.dump(note), "CONTENT_TYPE" => "application/json"
     assert last_response.status == 201
 
     new_note = MultiJson.load(last_response.body)
@@ -78,8 +92,22 @@ class TestNotesController < TestCase
   end
 
   def test_notes_for_ontology
+    get @@ontology.id.to_s
+    ont = MultiJson.load(last_response.body)
+    get ont["links"]["notes"]
+    notes = MultiJson.load(last_response.body)
+    test_note = notes.select {|n| n["subject"].eql?("Test subject 1")}
+    assert test_note.length == 1
+    assert notes.length >= 5
   end
 
   def test_notes_for_class
+    get "/ontologies/#{@@ontology.acronym}/classes/#{CGI.escape(@@cls.id.to_s)}"
+    cls = MultiJson.load(last_response.body)
+    get cls["links"]["notes"]
+    notes = MultiJson.load(last_response.body)
+    test_note = notes.select {|n| n["subject"].eql?("Test subject 1")}
+    assert test_note.length == 1
+    assert notes.length >= 5
   end
 end
