7e29063b09cfd7f7e84ea5ca29508bebd6a2f522
Paul R Alexander <palexander@stanford.edu>
2013-07-11 13:00:27 -0700

    Improve notes thread loading

--- helpers/notes_helper.rb
+++ helpers/notes_helper.rb
@@ -11,19 +11,11 @@ module Sinatra
         return if notes.nil?
         notes = notes.is_a?(Array) ? notes : [notes]
         if notes.first.is_a?(LinkedData::Models::Note)
-          notes = LinkedData::Models::Note.where.models(notes).include(reply: LinkedData::Models::Note.goo_attrs_to_load).all
           notes.each do |note|
-            # Handle notes
-            if !note.reply.compact.empty?
-              note.reply.each do |reply|
-                # reply = LinkedData::Models::Notes::Reply.find(reply.to_s).first
-                reply.bring_remaining
-                recurse_replies(reply)
-              end
-            end
+            recurse_replies(note.reply) unless note.reply.compact.empty?
           end
         elsif notes.first.is_a?(LinkedData::Models::Notes::Reply)
-          notes = LinkedData::Models::Notes::Reply.where.models(notes).include(*([:children] + LinkedData::Models::Note.goo_attrs_to_load)).all
+          notes = LinkedData::Models::Notes::Reply.where.models(notes).include(children: LinkedData::Models::Notes::Reply.goo_attrs_to_load).all
           notes.each do |note|
             recurse_replies(note.children)
           end
