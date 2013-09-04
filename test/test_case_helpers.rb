2e48b8a2ccf91475a3cad28d763d8b33fed24866
Paul R Alexander <palexander@stanford.edu>
2013-09-04 16:56:46 -0700

    Make helpers get included automatically

--- test/test_case_helpers.rb
+++ test/test_case_helpers.rb
@@ -7,9 +7,17 @@ require_relative "test_case"
 #   matches the name of the helper module exactly (including case).
 # Use 'rake test' from the command line to run tests.
 # See http://www.sinatrarb.com/testing.html for testing information
+
 class TestCaseHelpers < TestCase
+  include Sinatra::Helpers::ApplicationHelper
+  Sinatra::Helpers.constants.each do |helper|
+    helper = Sinatra::Helpers.const_get(helper)
+    include helper if helper.class == Module
+  end
+
   def helper
     class_name = self.class.name.gsub(/^Test/, "")
-    extend Kernel.const_get("Sinatra").const_get("Helpers").const_get(class_name)
+    helper_class = Kernel.const_get("Sinatra").const_get("Helpers").const_get(class_name)
+    extend helper_class
   end
 end
