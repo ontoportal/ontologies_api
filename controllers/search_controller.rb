d140e8b7b6078fbc35f197f9aeaa2e179d9ae667
Michael Dorf <mdorf@stanford.edu>
2013-09-20 15:37:20 -0700

    implemented NCBO-209 - Search should pass list of ontologies a user has access to

--- controllers/search_controller.rb
+++ controllers/search_controller.rb
@@ -22,16 +22,7 @@ class SearchController < ApplicationController
       q = params["q"]
       globalParams = @params.dup
       query = get_query(q, globalParams)
-
-
-
-
-      puts query
-
-
-
-
-
+      #puts query
       params = get_params(globalParams)
       docs = Array.new
 
