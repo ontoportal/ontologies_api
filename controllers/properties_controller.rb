54c8071fbdd077e39d6eca69be4202d0533f8d0c
msalvadores <msalvadores@gmail.com>
2013-09-24 15:57:35 -0700

    properties controller for simple label retrieval

---
+++ controllers/properties_controller.rb
@@ -0,0 +1,23 @@
+class PropertiesController < ApplicationController
+
+  namespace "/ontologies/:ontology/properties" do
+    get '/:property/label' do 
+      ont, submission = get_ontology_and_submission
+      query_label = <<eso
+SELECT * 
+FROM #{submission.id.to_ntriples}
+WHERE {
+<#{params[:property]}> <http://www.w3.org/2000/01/rdf-schema#label> ?label }
+ LIMIT 1
+eso
+      epr = Goo.sparql_query_client(:main)
+      label = nil
+      graphs = [submission.id]
+      epr.query(query_label, graphs: graphs, query_options: {rules: :NONE}).each do |sol|
+        label = sol[:label].object
+      end
+      reply({ label: label })
+    end
+  end
+
+end
