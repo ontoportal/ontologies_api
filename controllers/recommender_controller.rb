16b3c33dbd67242ff1fa171c5303045551187d2c
Michael Dorf <mdorf@stanford.edu>
2013-09-25 10:35:09 -0700

    added ontology restriction check to recommender

--- controllers/recommender_controller.rb
+++ controllers/recommender_controller.rb
@@ -5,17 +5,12 @@ class RecommenderController < ApplicationController
     get do
       text = params["text"]
       raise error 400, "A text to be analyzed by the recommender must be supplied using the argument text=<text>" if text.nil? || text.strip.empty?
-      ontologies = ontologies_param_to_acronyms
+      acronyms = restricted_ontologies_to_acronyms(params)
       recommender = Recommender::Models::NcboRecommender.new
-      recommendations = recommender.recommend(text, ontologies)
+      recommendations = recommender.recommend(text, acronyms)
       reply 200, recommendations
     end
 
-    private
-
-    def get_page_params(text, args={})
-      return args
-    end
   end
 end
 
