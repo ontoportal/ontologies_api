664177d35ddeb2e40522abe53f969a9a1496509d
Paul R Alexander <palexander@stanford.edu>
2013-09-25 23:03:04 -0700

    Admin endpoint to print out ObjectSpace stats

---
+++ controllers/admin_controller.rb
@@ -0,0 +1,20 @@
+require 'multi_json'
+
+class AdminController < ApplicationController
+
+  namespace "/admin" do
+    before {
+      if LinkedData.settings.enable_security && (!env["REMOTE_USER"] || !env["REMOTE_USER"].admin?)
+        error 403, "Access denied"
+      end
+    }
+
+    get "/objectspace" do
+      GC.start
+      gdb_objs = Hash.new 0
+      ObjectSpace.each_object {|o| gdb_objs[o.class] += 1}
+      obj_usage = gdb_objs.to_a.sort {|a,b| b[1]<=>a[1]}
+      MultiJson.dump obj_usage
+    end
+  end
+end
