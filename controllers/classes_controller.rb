5d37252ab5762925d59560ef11ba1d96b99c7377
Paul R Alexander <palexander@stanford.edu>
2013-10-08 14:49:38 -0700

    Force /tree call to serialize children on nested objects

--- controllers/classes_controller.rb
+++ controllers/classes_controller.rb
@@ -93,6 +93,7 @@ class ClassesController < ApplicationController
       includes_param_check
       # We override include values other than the following, user-provided include ignored
       params["include"] = "prefLabel,childrenCount,children"
+      params["serialize_nested"] = true # Override safety check and cause children to serialize
       env["rack.request.query_hash"] = params
 
       ont, submission = get_ontology_and_submission
