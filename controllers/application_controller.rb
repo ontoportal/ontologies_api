c6a82dda66cc176a46eba40ab53596fa075749f4
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2013-10-15 11:00:48 -0700

    NCBO-331 #comment Move download restrictions to configuration

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -4,24 +4,9 @@ class ApplicationController
   include Sinatra::Delegator
   extend Sinatra::Delegator
 
-  # TODO: Also check for licensing restrictions, see
-  # TODO: https://bmir-jira.stanford.edu/browse/NCBO-331
-  # TODO: Revise this code if/when the ontology model contains license attributes
-  # TODO: to be used for access filtering on downloads.
+  # Restrict downloads for ontologies with licensing restrictions.
   # Used in ontologies_controller and ontology_submissions_controller
-  ONT_RESTRICT_DOWNLOADS = ['NDFRT',
-                            'SNOMEDCT',
-                            'WHO-ART',
-                            'NDDF',
-                            'MDDB',
-                            'RCD',
-                            'NIC',
-                            'ICPC2P',
-                            'SCTSPA',
-                            'MSHSPA_1',
-                            'MEDDRA',
-                            'NDDF',
-                            'MSHFRE']
+  ONT_RESTRICT_DOWNLOADS = LinkedData::OntologiesAPI.settings.restrict_download
 
   # Run before route
   before {
