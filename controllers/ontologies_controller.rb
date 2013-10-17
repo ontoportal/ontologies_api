2faf20b13814d8a4d24aba86a8abc670ec76e446
Paul R Alexander <palexander@stanford.edu>
2013-10-16 18:13:22 -0700

    Check last modified for submission, not ontology

--- controllers/ontologies_controller.rb
+++ controllers/ontologies_controller.rb
@@ -43,13 +43,13 @@ class OntologiesController < ApplicationController
       ont = Ontology.find(params["acronym"]).first
       error 404, "You must provide a valid `acronym` to retrieve an ontology" if ont.nil?
       include_status = params["include_status"]
-      check_last_modified(ont)
       ont.bring(:acronym, :submissions)
       if include_status
         latest = ont.latest_submission(status: include_status.to_sym)
       else
         latest = ont.latest_submission(status: :any)
       end
+      check_last_modified(latest)
       latest.bring(*OntologySubmission.goo_attrs_to_load(includes_param)) if latest
       reply(latest || {})
     end
