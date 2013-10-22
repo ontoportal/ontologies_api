d3a938d160d70f1ca8793be8f1826c11624d67b1
Paul R Alexander <palexander@stanford.edu>
2013-10-22 14:42:55 -0700

    Disable miniprofiler by default

--- config/config.rb
+++ config/config.rb
@@ -22,6 +22,7 @@ module LinkedData::OntologiesAPI
     @settings.resolver_redis_host   ||= "localhost"
     @settings.resolver_redis_port   ||= 6379
     @settings.restrict_download     ||= []
+    @settings.enable_miniprofiler   ||= false
 
     if @settings.enable_monitoring
       puts "(API) >> Slow queries log enabled: #{@settings.slow_request_log}"
