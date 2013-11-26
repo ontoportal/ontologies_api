1ea6772143290fdd480e1b003490eb4da0886b15
Darren L. Weber, Ph.D <darren.weber@stanford.edu>
2013-11-26 10:42:11 -0800

    Ontology diffs - possible test code for downloads.

--- test/controllers/test_ontology_submissions_controller.rb
+++ test/controllers/test_ontology_submissions_controller.rb
@@ -176,6 +176,9 @@ class TestOntologySubmissionsController < TestCase
     # see also test_ontologies_controller::test_download_ontology
   end
 
+  #
+  # NOTE: download restrictions are tested in the download test above.
+  #
   #def test_download_restricted_submission
   #  num_onts_created, created_ont_acronyms, onts = create_ontologies_and_submissions(ont_count: 1, submission_count: 1, process_submission: true)
   #  assert_equal(1, num_onts_created, msg="Failed to create 1 ontology?")
@@ -195,6 +198,35 @@ class TestOntologySubmissionsController < TestCase
   #  # Clear restrictions on downloads
   #  LinkedData::OntologiesAPI.settings.restrict_download = []
   #  # see also test_ontologies_controller::test_download_restricted_ontology
+  #end
+
+  #
+  # TODO: Test the submission diff file download
+  #
+  #def test_download_submission_diff
+  #  num_onts_created, created_ont_acronyms, onts = create_ontologies_and_submissions(ont_count: 1, submission_count: 1, process_submission: true)
+  #  assert_equal(1, num_onts_created, msg="Failed to create 1 ontology?")
+  #  assert_equal(1, onts.length, msg="Failed to create 1 ontology?")
+  #  ont = onts.first
+  #  assert_instance_of(Ontology, ont, msg="ont is not a #{Ontology.class}")
+  #  assert_equal(1, ont.submissions.length, msg="Failed to create 1 ontology submission?")
+  #  sub = ont.submissions.first
+  #  assert_instance_of(OntologySubmission, sub, msg="sub is not a #{OntologySubmission.class}")
+  #  # Clear restrictions on downloads
+  #  LinkedData::OntologiesAPI.settings.restrict_download = []
+  #  # Download the specific submission
+  #  get "/ontologies/#{sub.ontology.acronym}/submissions/#{sub.submissionId}/download"
+  #  assert_equal(200, last_response.status, msg='failed download for specific submission : ' + get_errors(last_response))
+  #  # Add restriction on download
+  #  acronym = created_ont_acronyms.first
+  #  LinkedData::OntologiesAPI.settings.restrict_download = [acronym]
+  #  # Try download
+  #  get "/ontologies/#{sub.ontology.acronym}/submissions/#{sub.submissionId}/download"
+  #  # download should fail with a 403 status
+  #  assert_equal(403, last_response.status, msg='failed to restrict download for ontology : ' + get_errors(last_response))
+  #  # Clear restrictions on downloads
+  #  LinkedData::OntologiesAPI.settings.restrict_download = []
+  #  # see also test_ontologies_controller::test_download_ontology
   #end
 
   def test_ontology_submission_properties
