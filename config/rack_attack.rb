c4dc9bc5266f90b4eebdf74ea6696ff5c1779d41
Paul R Alexander <palexander@stanford.edu>
2014-03-27 14:23:38 -0700

    Whitelist biomixer user account

--- config/rack_attack.rb
+++ config/rack_attack.rb
@@ -8,8 +8,9 @@ attack_redis_host_port = {host: LinkedData::OntologiesAPI.settings.http_redis_ho
 attack_store = ActiveSupport::Cache::RedisStore.new(attack_redis_host_port)
 Rack::Attack.cache.store = attack_store
 
+RACK_ATTACK_EXCEPTIONS = Set.new(["ncbobioportal", "biomixer"])
 Rack::Attack.whitelist('always allow') do |req|
-  req.env["REMOTE_USER"] && (req.env["REMOTE_USER"].username == "ncbobioportal" || req.env["REMOTE_USER"].admin?)
+  req.env["REMOTE_USER"] && (RACK_ATTACK_EXCEPTIONS.include?(req.env["REMOTE_USER"].username) || req.env["REMOTE_USER"].admin?)
 end
 
 Rack::Attack.throttle('req/ip', :limit => LinkedData::OntologiesAPI.settings.req_per_second_per_ip, :period => 1.second) do |req|
