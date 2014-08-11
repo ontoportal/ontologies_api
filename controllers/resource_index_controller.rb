c611028a2f9011ec9d2511c0cc3256d620c9314e
Paul R Alexander <palexander@stanford.edu>
2014-08-11 10:44:48 -0700

    Initial version of Resource Index controller

--- controllers/resource_index_controller.rb
+++ controllers/resource_index_controller.rb
@@ -5,41 +5,42 @@ class ResourceIndexController < ApplicationController
 
   namespace "/resource_index" do
 
-    get '/search' do
-      options = get_options(params)
+    get do
+    end
+
+    get '/counts' do
+      format_params(params)
       classes = get_classes(params)
       error 404, "You must provide valid `classes` to retrieve resources" if classes.empty?
-
-      search_array = massage_search(result, options)
-      page = page_object(search_array)
-      reply page
+      reply classes.ri_counts(params["resources"])
     end
 
     # Return all resources
     get "/resources" do
       # expires TWENTYFOUR_HOURS, :public
-      options = get_options(params)
-      result = NCBO::ResourceIndex.resources(options)
-      check404(result, "No resources found.")
-      reply massage_resources(result)
+      reply RI::Resource.populated
     end
 
     # Return specific resources
     get "/resources/:resources" do
-      options = get_options(params)
-      result = NCBO::ResourceIndex.resources_hash(options)
-      check404(result, "No resources found.")
-      resources_filtered = []
-      options[:resourceids].each do |r|
-        rid = r.downcase.to_sym
-        resources_filtered.push result[rid] if result.keys.include? rid
-      end
-      reply massage_resources(resources_filtered)
+      format_params(params)
+      resources = params["resources"].map {|res_id| RI::Resource.find(res_id)}.compact.sort {|a,b| a.name.downcase <=> b.name.downcase}
+      error 404, "Could not find resource #{params['resources'].join(', ')}" if resources.empty?
+      reply resources
+    end
+
+    get '/resources/:resources/search' do
+      format_params(params)
+      classes = get_classes(params)
+      error 404, "You must provide valid `classes` to retrieve resources" if classes.empty?
+      error 422, "You may only specify a single resource" if params["resources"].length > 1
+      resource = params["resources"].first
+      reply classes.ri_docs_page(resource, params)
     end
 
     # Return a specific element from a specific resource
     get "/resources/:resources/elements/:elements" do
-      options = get_options(params)
+      format_params(params)
       result = NCBO::ResourceIndex.element(params["elements"], params["resources"], options)
       check404(result, "No element found.")
       element = massage_element(result, options[:elementDetails])
--- helpers/resource_index_helper.rb
+++ helpers/resource_index_helper.rb
@@ -19,36 +19,31 @@ module Sinatra
         if params.key?("classes")
           classes_error(params) if not params["classes"].kind_of? Hash
           class_hash = params["classes"]
-          class_hash.each do |k,v|
+          class_hash.each do |ont_id, cls_ids|
             # Use 'k' as an ontology acronym or URI, translate it to an ontology virtual ID.
-            ont_id = virtual_id_from_acronym(k)
-            ont_id = virtual_id_from_uri(k) if ont_id.nil?
-            msg = "Ontology #{k} cannot be found in the resource index. "
-            msg += "See #{LinkedData.settings.rest_url_prefix}resource_index/ontologies for details."
-            error 404, msg if ont_id.nil?
-            classes_error(params) if not v.kind_of? String
-            # Use 'v' as a CSV list of concepts (class ID)
-            v.split(',').each do |class_id|
-              # Shorten id, if necessary
-              if class_id.start_with?("http://")
-                class_id = short_id_from_uri(class_id, ont_id)
-              end
-              # TODO: Determine whether class_id exists, throw 404 for invalid classes.
-              classes.push("#{ont_id}/#{class_id}")
+            ont = LinkedData::Models::Ontology.find(uri_as_needed(ont_id)).include(:acronym).first
+            error 404, "Ontology #{ont_id} cannot be found" if ont.nil?
+            sub = ont.latest_submission
+            sub.ontology = ont
+            classes_error(params) if not cls_ids.kind_of? String
+
+            cls_ids.split(',').each do |cls_id|
+              params[:cls] = cls_id # needed for get_class
+              cls = get_class(sub)
+              classes.push(cls) if cls
+              params.delete(:cls) # not used after get_class
             end
           end
         end
-        return classes
+
+        classes
       end
 
-      def get_options(params={})
-        options = {}
-
+      def format_params(params={})
         page, page_size = page_params
-
         offset, limit = offset_and_limit(page, page_size)
-        options[:offset] = offset unless offset.nil?
-        options[:limit] = limit unless limit.nil?
+        params[:from] = offset unless offset.nil?
+        params[:size] = limit unless limit.nil?
 
         if params["elements"].is_a? String
           params["elements"] = params["elements"].split(',')
@@ -66,7 +61,7 @@ module Sinatra
           params['semantic_types'] = params['semantic_types'].split(',')
         end
 
-        return options
+        nil
       end
     end
   end
