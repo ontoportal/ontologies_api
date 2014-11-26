2cb3abca3d56d5755eab29a8332a970514e82ebc
msalvadores <msalvadores@gmail.com>
2014-11-26 11:44:31 -0800

    1091 rest endpoint for ontology report

--- controllers/admin_controller.rb
+++ controllers/admin_controller.rb
@@ -16,5 +16,17 @@ class AdminController < ApplicationController
       obj_usage = gdb_objs.to_a.sort {|a,b| b[1]<=>a[1]}
       MultiJson.dump obj_usage
     end
+
+    get "/report" do
+      report_path = LinkedData::OntologiesAPI.settings.ontology_report_path
+      if report_path.nil? or report_path.length == 0
+        reply({ error: "report path not set in config" })
+      end
+      if !File.exist?(report_path)
+        reply({ error: "file #{report_path} not found"})
+      end
+      json_string = IO.read(report_path)
+      reply JSON.parse(json_string)
+    end
   end
 end
