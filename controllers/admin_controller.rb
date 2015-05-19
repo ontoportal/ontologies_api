d8ec77c52827ee4a380f10f3a9a72adff2de1b42
mdorf <mdorf@stanford.edu>
2015-05-18 18:21:36 -0700

    implemented ontologies report refresh functionality in Admin - NCBO-1363

--- controllers/admin_controller.rb
+++ controllers/admin_controller.rb
@@ -17,12 +17,6 @@ class AdminController < ApplicationController
       MultiJson.dump obj_usage
     end
 
-    get "/ontologies_report" do
-      refresh_ontologies_report if params["refresh"].eql?('true') # default = false
-      suppress_error = params["suppress_error"].eql?('true') # default = false
-      reply raw_ontologies_report(suppress_error)
-    end
-
     get "/ontologies/:acronym/log" do
       ont = Ontology.find(params["acronym"]).first
       error 404, "Ontology #{params["acronym"]} does not exist" if ont.nil?
@@ -32,5 +26,65 @@ class AdminController < ApplicationController
       reply get_parse_log_file(latest)
     end
 
+    get "/ontologies_report" do
+      suppress_error = params["suppress_error"].eql?('true') # default = false
+      reply raw_ontologies_report(suppress_error)
+    end
+
+    post "/ontologies_report" do
+      args = {name: "ontologies_report", message: "refreshing ontologies report"}
+      process_id = process_long_operation(900, args) do |args|
+        refresh_ontologies_report
+      end
+      reply(process_id: process_id)
+    end
+
+    get "/ontologies_report/:process_id" do
+      process_id = MultiJson.load(redis.get(params["process_id"]))
+
+      if process_id.nil?
+        error 404, "Process id #{params["process_id"]} does not exit"
+      else
+        if process_id === "done"
+          reply raw_ontologies_report(false)
+        else
+          # either "processing" OR errors {errors: ["errorA", "errorB"]}
+          reply process_id
+        end
+      end
+    end
+
+    private
+
+    def process_long_operation(timeout, args)
+      process_id = "#{Time.now.to_i}_#{args[:name]}"
+      redis.setex process_id, timeout, MultiJson.dump("processing")
+      proc = Proc.new {
+        error = {}
+        begin
+          yield(args)
+        rescue Exception => e
+          msg = "Error #{args[:message]}: #{e.message}"
+          puts msg
+          error[:errors] = [msg]
+        end
+        redis.setex process_id, timeout, MultiJson.dump(error.empty? ? "done" : error)
+      }
+
+      fork = true # set to false for testing
+      if fork
+        pid = Process.fork do
+          proc.call
+        end
+        Process.detach(pid)
+      else
+        proc.call
+      end
+      process_id
+    end
+
+    def redis
+      Redis.new(host: Annotator.settings.annotator_redis_host, port: Annotator.settings.annotator_redis_port)
+    end
   end
 end
