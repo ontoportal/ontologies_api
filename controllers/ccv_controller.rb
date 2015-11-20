a1aad18531a5f61275393a0d43696c4ccb089c18
mdorf <mdorf@stanford.edu>
2015-11-19 22:43:10 -0800

    made loading parents/children, analytics and images optional parameters for concept-centeric-view (hackathon)

--- controllers/ccv_controller.rb
+++ controllers/ccv_controller.rb
@@ -11,32 +11,31 @@ class CCVController < ApplicationController
   namespace "/ccv" do
 
     get do
-
-      process_search()
-
+      reply 200, process_concept_search
     end
 
-    def process_search(params=nil)
+    private
+
+    def process_concept_search(params=nil)
       ont_url = lambda { |acronym| "http://bioportal.bioontology.org/ontologies/#{acronym}" }
       concept_url = lambda { |acronym, uri| "#{ont_url.call(acronym)}?p=classes&conceptid=#{CGI.escape(uri)}" }
-
 
       params ||= @params
       params["exact_match"] = true
       text = params["q"]
 
+      include_family = params["include_family"].eql?('true') # default = false
+      include_analytics = params["include_analytics"].eql?('true') # default = false
+      include_images = params["include_images"].eql?('true') # default = false
+
       query = get_edismax_query(text, params)
       set_page_params(params)
 
-      docs = Array.new
-      loaded_ontologies_submissions = Hash.new
+      loaded_submissions = Hash.new
       loaded_ontologies = Array.new
       resp = LinkedData::Models::Class.search(query, params)
       total_found = resp["response"]["numFound"]
-
-
-      agg_doc = {"term" => text, "ids" => [], "ontologies" => [], "definitions" => [], "synonyms" => [], "parents" => [], "children" => [], "analytics" => Hash.new}
-
+      agg_doc = {"term" => text, "ids" => [], "ontologies" => []}
 
       all_synonyms = Hash.new
       all_definitions = Hash.new
@@ -46,71 +45,45 @@ class CCVController < ApplicationController
       resp["response"]["docs"].each do |doc|
         doc = doc.symbolize_keys
         resource_id = doc[:resource_id]
-        # doc.delete :resource_id
-        # doc[:id] = resource_id
+        acronym = doc[:submissionAcronym]
         ontology_uri = doc[:ontologyId].first.sub(/\/submissions\/.*/, "")
-
-        # doc.delete :prefLabelExact
-        # doc.delete :prefLabelSuggest
-        # doc.delete :prefLabelSuggestEdge
-        # doc.delete :prefLabelSuggestNgram
-        # doc.delete :synonymExact
-        # doc.delete :synonymSuggest
-        # doc.delete :synonymSuggestEdge
-        # doc.delete :synonymSuggestNgram
-        # doc.delete :childCount
-        # doc.delete :notation
-        # doc.delete :property
-        # doc.delete :propertyRaw
-        # doc.delete :_version_
-        # doc.delete :ontologyType
-        # doc.delete :obsolete
-        # doc.delete :provisional
-        # doc.delete :ontologyId
-
-        ontology_rank = LinkedData::OntologiesAPI.settings.ontology_rank[doc[:submissionAcronym]] || 0
-        # doc[:ontology] = {"id" => ontology_uri, "acronym" => doc[:submissionAcronym], "rank" => ontology_rank}
+        ontology_rank = LinkedData::OntologiesAPI.settings.ontology_rank[acronym] || 0
         doc[:synonym] ||= []
         doc[:definition] ||= []
 
-        ont, sub = ontology_and_submission(doc[:submissionAcronym], loaded_ontologies_submissions)
-        cls_info = get_class_info(resource_id, sub)
-        c_info = cls_info
+        agg_doc["ids"] << {"id" => resource_id, "ui" => concept_url.call(acronym, resource_id)}
 
-        # doc[:properties] = MultiJson.load(doc.delete(:propertyRaw)) if include_param_contains?(:properties)
-
-
-        # docs.push(doc)
-
-
-
-
-        agg_doc["ids"] << {"id" => resource_id, "ui" => concept_url.call(doc[:submissionAcronym], resource_id)}
-
-        if (!loaded_ontologies.include?(doc[:submissionAcronym]))
-          ont_doc = {"id" => ontology_uri, "acronym" => doc[:submissionAcronym], "rank" => ontology_rank, "ui" => ont_url.call(doc[:submissionAcronym])}
+        if (!loaded_ontologies.include?(acronym))
+          ont_doc = {"id" => ontology_uri, "acronym" => acronym, "rank" => ontology_rank, "ui" => ont_url.call(acronym)}
           agg_doc["ontologies"] << ont_doc
-          loaded_ontologies << doc[:submissionAcronym]
+          loaded_ontologies << acronym
         end
 
         aggregate_vals(all_synonyms, doc[:synonym], "synonym")
         aggregate_vals(all_definitions, doc[:definition], "definition")
-        aggregate_vals(all_parents, c_info[:parents], "term")
-        aggregate_vals(all_children, c_info[:children], "term")
+
+        if include_family
+          sub = submission(acronym, loaded_submissions)
+          cls_family = class_family(resource_id, sub)
+          aggregate_vals(all_parents, cls_family[:parents], "term")
+          aggregate_vals(all_children, cls_family[:children], "term")
+        end
+
       end
 
       agg_doc["synonyms"] = all_synonyms.values
       agg_doc["definitions"] = all_definitions.values
-      agg_doc["parents"] = all_parents.values
-      agg_doc["children"] = all_children.values
 
-      agg_doc["analytics"] = analytics(text)
+      if include_family
+        agg_doc["parents"] = all_parents.values
+        agg_doc["children"] = all_children.values
+      end
 
-      reply 200, agg_doc
+      agg_doc["analytics"] = analytics(text) if include_analytics
+      agg_doc["images"] = wikipedia_images(text) if include_images
+
+      agg_doc
     end
-
-
-
 
     def aggregate_vals(master_hash, data, label)
       data.each do |k|
@@ -124,8 +97,8 @@ class CCVController < ApplicationController
       end
     end
 
-    def get_class_info(cls_id, submission)
-      cls_info = {parents: [], children: []}
+    def class_family(cls_id, submission)
+      cls_family = {parents: [], children: []}
 
       unless submission.nil?
         cls = LinkedData::Models::Class.find(RDF::URI.new(cls_id)).in(submission).first
@@ -136,31 +109,29 @@ class CCVController < ApplicationController
 
           cls.parents.each do |p|
             begin
-              cls_info[:parents] << p.prefLabel
+              cls_family[:parents] << p.prefLabel
             rescue Exception => e
             end
           end
 
           cls.children.each do |c|
             begin
-              cls_info[:children] << c.prefLabel
+              cls_family[:children] << c.prefLabel
             rescue Exception => e
             end
           end
         end
       end
-      cls_info
+      cls_family
     end
 
-
-    def ontology_and_submission(acronym, loaded_ontologies_submissions)
-      return loaded_ontologies_submissions[acronym] if loaded_ontologies_submissions.has_key?(acronym)
+    def submission(acronym, loaded_submissions)
+      return loaded_submissions[acronym] if loaded_submissions.has_key?(acronym)
       sub = nil
       ont = Ontology.find(acronym).include(submissions: [:submissionId, submissionStatus: [:code], ontology: [:acronym]]).first
-      sub = ont.latest_submission(status: [:RDF]) unless ont.nil?
-      ont_sub_arr = [ont, sub]
-      loaded_ontologies_submissions[acronym] = ont_sub_arr
-      ont_sub_arr
+      sub = ont.latest_submission(status: :rdf) unless ont.nil?
+      loaded_submissions[acronym] = sub
+      sub
     end
 
     def analytics(query)
@@ -168,7 +139,7 @@ class CCVController < ApplicationController
       start_index = 1
       aggregated_results = Hash.new
       start_date = "2014-01-01"
-      start_year = 2015
+      start_year = 2014
 
       google_client = authenticate_google
       api_method = google_client.discovered_api('analytics', 'v3').data.ga.get
@@ -229,6 +200,35 @@ class CCVController < ApplicationController
       client.authorization.fetch_access_token!
       client
     end
+
+    def wikipedia_images(query)
+      ignore_images = ["File:Closed Access logo alternative.svg", "File:Commons-logo.svg"]
+      images_url = "https://en.wikipedia.org/w/api.php?action=query&titles=#{CGI.escape(query)}&prop=images&format=json&imlimit=10"
+      single_image_url = lambda { |i| "https://en.wikipedia.org/w/api.php?action=query&titles=#{i}&prop=imageinfo&iiprop=url&format=json" }
+      resp_raw = Net::HTTP.get_response(URI.parse(images_url))
+      resp = MultiJson.load(resp_raw.body)
+      images = []
+
+      if resp["query"]["pages"]
+        img_hash = resp["query"]["pages"][resp["query"]["pages"].keys[0]]
+
+        if img_hash["images"] && !img_hash["images"].empty?
+          img_hash["images"].each do |i|
+            next if ignore_images.include?(i["title"])
+            img_title = CGI.escape(i["title"])
+            resp_img_raw = Net::HTTP.get_response(URI.parse(single_image_url.call(img_title)))
+            resp_img = MultiJson.load(resp_img_raw.body)
+
+            if resp_img["query"]["pages"]
+              img_info_hash = resp_img["query"]["pages"][resp_img["query"]["pages"].keys[0]]
+              images << img_info_hash["imageinfo"][0]["url"]
+            end
+          end
+        end
+      end
+      images
+    end
+
   end
 
 end
