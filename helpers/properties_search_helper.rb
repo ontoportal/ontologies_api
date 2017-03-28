4583295cc377f3771156bf708dc6f1a9ee141fd6
mdorf <mdorf@stanford.edu>
2017-03-28 13:45:35 -0700

    implemented a basic property search controller

---
+++ helpers/properties_search_helper.rb
@@ -0,0 +1,57 @@
+require 'sinatra/base'
+require 'multi_json'
+
+module Sinatra
+  module Helpers
+    module PropertiesSearchHelper
+      ALLOWED_INCLUDES_PARAMS = [:label, :labelGenerated, :definition].freeze
+
+      def get_properties_search_query(text, params)
+        validate_params_solr_population(ALLOWED_INCLUDES_PARAMS)
+
+        query = ""
+        params["defType"] = "edismax"
+        params["stopwords"] = "true"
+        params["lowercaseOperators"] = "true"
+        params["fl"] = "*,score"
+
+        # highlighting is used to determine the field that got matched
+        params["hl"] = "on"
+        params["hl.simple.pre"] = SearchHelper::MATCH_HTML_PRE
+        params["hl.simple.post"] = SearchHelper::MATCH_HTML_POST
+
+        if params[SearchHelper::EXACT_MATCH_PARAM] == "true"
+          query = "\"#{solr_escape(text)}\""
+          params["qf"] = "resource_id^20 labelExact^10 labelGeneratedExact^8"
+          params["hl.fl"] = "resource_id labelExact labelGeneratedExact"
+        else
+          params["qf"] = "labelExact^100 labelGeneratedExact^80 labelSuggestEdge^50 labelSuggestNgram resource_id"
+          query = solr_escape(text)
+        end
+
+        params["ontologies"] = params["ontology_acronyms"].join(",") if params["ontology_acronyms"] && !params["ontology_acronyms"].empty?
+        ontology_types = params[SearchHelper::ONTOLOGY_TYPES_PARAM].nil? || params[SearchHelper::ONTOLOGY_TYPES_PARAM].empty? ? [] : params[SearchHelper::ONTOLOGY_TYPES_PARAM].split(",").map(&:strip)
+        onts = restricted_ontologies(params)
+
+        onts.select! { |o| ont_type = o.ontologyType.nil? ? "ONTOLOGY" : o.ontologyType.get_code_from_id; ontology_types.include?(ont_type) } unless ontology_types.empty?
+        acronyms = restricted_ontologies_to_acronyms(params, onts)
+        filter_query = get_quoted_field_query_param(acronyms, "OR", "submissionAcronym")
+
+        ontology_types_clause = ontology_types.empty? ? "" : get_quoted_field_query_param(ontology_types, "OR", "ontologyType")
+        filter_query = "#{filter_query} AND #{ontology_types_clause}" unless (ontology_types_clause.empty?)
+        filter_query << " AND definition:[* TO *]" if params[SearchHelper::REQUIRE_DEFINITIONS_PARAM] == "true"
+
+        params["fq"] = filter_query
+        params["q"] = query
+        query
+      end
+
+      def property_object_instance(type)
+        Object.const_get("LinkedData::Models::#{type.capitalize}Property")
+      end
+
+    end
+  end
+end
+
+helpers Sinatra::Helpers::PropertiesSearchHelper
