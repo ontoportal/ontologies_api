ccc8c8517449d7dd4ba929d470d142293373c1f1
mdorf <mdorf@stanford.edu>
2017-04-14 16:40:03 -0700

    unit tests for property roots and property tree endpoints

--- test/controllers/test_properties_controller.rb
+++ test/controllers/test_properties_controller.rb
@@ -52,4 +52,81 @@ class TestPropertiesController < TestCase
     get "/ontologies/#{@@acronyms.first}/properties/http%3A%2F%2Fbioontology.org%2Fontologies%2FBiomedicalResourceOntology.owl%23DummyProp"
     assert_equal 404, last_response.status
   end
+
+  def test_property_roots
+    get "/ontologies/#{@@acronyms.first}/properties/roots"
+    assert last_response.ok?
+    pr = MultiJson.load(last_response.body)
+    assert_equal 61, pr.length
+
+    # count object properties
+    opr = pr.select { |p| p["@type"] == "http://www.w3.org/2002/07/owl#ObjectProperty" }
+    assert_equal 18, opr.length
+    # count datatype properties
+    dpr = pr.select { |p| p["@type"] == "http://www.w3.org/2002/07/owl#DatatypeProperty" }
+    assert_equal 32, dpr.length
+    # count annotation properties
+    apr = pr.select { |p| p["@type"] == "http://www.w3.org/2002/07/owl#AnnotationProperty" }
+    assert_equal 11, apr.length
+    # check for non-root properties
+    assert_empty pr.select { |p| ["http://www.w3.org/2004/02/skos/core#broaderTransitive",
+                                  "http://www.w3.org/2004/02/skos/core#topConceptOf",
+                                  "http://www.w3.org/2004/02/skos/core#relatedMatch",
+                                  "http://www.w3.org/2004/02/skos/core#exactMatch",
+                                  "http://www.w3.org/2004/02/skos/core#narrowMatch"].include?(p["@id"]) },
+                 "Ontology #{@@acronyms.first}: Non-root nodes found in where roots are expected"
+
+    get "/ontologies/#{@@acronyms.last}/properties/roots"
+    assert last_response.ok?
+    pr = MultiJson.load(last_response.body)
+    assert_equal 33, pr.length
+
+    # count object properties
+    opr = pr.select { |p| p["@type"] == "http://www.w3.org/2002/07/owl#ObjectProperty" }
+    assert_equal 22, opr.length
+    # count datatype properties
+    dpr = pr.select { |p| p["@type"] == "http://www.w3.org/2002/07/owl#DatatypeProperty" }
+    assert_equal 5, dpr.length
+    # count annotation properties
+    apr = pr.select { |p| p["@type"] == "http://www.w3.org/2002/07/owl#AnnotationProperty" }
+    assert_equal 6, apr.length
+
+    # check for non-root properties
+    assert_empty pr.select { |p| ["http://www.semanticweb.org/ontologies/2009/9/12/Ontology1255323704656.owl#overExpress",
+                                  "http://www.semanticweb.org/ontologies/2009/9/12/Ontology1255323704656.owl#underExpress"].include?(p["@id"]) },
+                 "Ontology #{@@acronyms.last}: Non-root nodes found in where roots are expected"
+  end
+
+  def test_property_tree
+    get "/ontologies/#{@@acronyms.first}/properties/http%3A%2F%2Fwww.w3.org%2F2004%2F02%2Fskos%2Fcore%23topConceptOf/tree"
+
+    assert last_response.ok?
+    pr = MultiJson.load(last_response.body)
+    assert_equal 61, pr.length
+    num_found = 0
+
+    pr.each do |p|
+      if p["@id"] == "http://www.w3.org/2004/02/skos/core#inScheme"
+        num_found += 1
+        assert p["hasChildren"]
+        assert_equal 1, p["children"].length
+        assert_equal ["is top concept in scheme"], p["children"][0]["label"]
+        assert_equal true, p["hasChildren"]
+        assert_empty p["children"][0]["children"]
+      end
+
+      if p["@id"] == "http://bioontology.org/ontologies/BiomedicalResourceOntology.owl#Identifier"
+        num_found += 1
+        assert_equal false, p["hasChildren"]
+        assert_empty p["children"]
+        assert_equal "http://www.w3.org/2002/07/owl#DatatypeProperty", p["@type"]
+        assert_equal ["Identifier"], p["label"]
+      end
+
+      break if num_found == 2
+    end
+
+    assert_equal 2, num_found
+  end
+
 end
