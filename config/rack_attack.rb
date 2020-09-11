c10b4fb65c6bcbf734fc877b57208ebe281ef5bf
Alex Skrenchuk <alexskr@stanford.edu>
2020-09-11 11:08:19 -0700

    set RateLimit headers for well-behaved clients

--- config/rack_attack.rb
+++ config/rack_attack.rb
@@ -4,7 +4,10 @@ require 'rack/attack'
 require 'redis-activesupport'
 use Rack::Attack
 
-attack_redis_host_port = {host: LinkedData::OntologiesAPI.settings.http_redis_host, port: LinkedData::OntologiesAPI.settings.http_redis_port}
+attack_redis_host_port = {
+  host: LinkedData::OntologiesAPI.settings.http_redis_host,
+  port: LinkedData::OntologiesAPI.settings.http_redis_port
+}
 attack_store = ActiveSupport::Cache::RedisStore.new(attack_redis_host_port)
 Rack::Attack.cache.store = attack_store
 
@@ -13,21 +16,26 @@ safe_ips.each do |safe_ip|
   Rack::Attack.safelist_ip(safe_ip)
 end
 
-safe_accounts = Set.new(["ncbobioportal", "biomixer"])
-Rack::Attack.safelist("mark ncbobioportal and biomixer as safe") do |req|
+safe_accounts = Set.new(['ncbobioportal', 'ontoportal_ui', 'biomixer'])
+Rack::Attack.safelist('mark ncbobioportal and biomixer as safe') do |req|
   req.env["REMOTE_USER"] && (safe_accounts.include?(req.env["REMOTE_USER"].username))
 end
 
-Rack::Attack.safelist("mark administrators as safe") do |req|
+Rack::Attack.safelist('mark administrators as safe') do |req|
   req.env["REMOTE_USER"] && req.env["REMOTE_USER"].admin?
 end
 
-Rack::Attack.throttle('req/ip', :limit => LinkedData::OntologiesAPI.settings.req_per_second_per_ip, :period => 1.second) do |req|
+Rack::Attack.throttle('req/ip', limit: LinkedData::OntologiesAPI.settings.req_per_second_per_ip, period: 1.second) do |req|
   req.ip
 end
 
 Rack::Attack.throttled_response = lambda do |env|
   data = env['rack.attack.match_data']
+  headers = {
+    'RateLimit-Limit' => data[:limit].to_s,
+    'RateLimit-Remaining' => '0',
+    'RateLimit-Reset' => data[:period].to_s
+  }
   body = "You have made #{data[:count]} requests in the last #{data[:period]} seconds. For user #{env["REMOTE_USER"]}, we limit API Keys to #{data[:limit]} requests every #{data[:period]} seconds"
-  [429, {}, [body]]
+  [429, headers, [body]]
 end
