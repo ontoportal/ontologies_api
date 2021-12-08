7dc7a6bdd900c8225bcde3fae7f893422a63c504
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2021-12-08 16:07:12 +0100

    Use directely the Istance model unstead the InstanceLoader in instances_controller.rb

--- controllers/instances_controller.rb
+++ controllers/instances_controller.rb
@@ -2,22 +2,39 @@ class InstancesController < ApplicationController
 
   # Display individuals for a class
   get '/ontologies/:ontology/classes/:cls/instances' do
+
     ont, sub = get_ontology_and_submission
-    check_last_modified_segment(LinkedData::Models::Class, [ont.acronym])
+    check_last_modified_segment(LinkedData::Models::Instance, [ont.acronym])
     cls = get_class(sub)
     error 404 if cls.nil?
     page, size = page_params
-    reply LinkedData::InstanceLoader.get_instances_by_class(sub.id, cls.id, page, size)
+
+    unmapped =  (includes_param && includes_param.include?(:all))
+    page_data = LinkedData::Models::Instance.where(cls.id.nil? ? nil :{types: RDF::URI.new(cls.id.to_s)}).in(sub)
+                                            .include(LinkedData::Models::Instance.attributes).page(page,size).all
+
+    if unmapped && page_data.length > 0
+      LinkedData::Models::Instance.in(sub).models(page_data).include(:unmapped).all
+    end
+    reply page_data
   end
 
   namespace "/ontologies/:ontology/instances" do
-
     # Display individuals for an ontology
     get do
       ont, sub = get_ontology_and_submission
       check_last_modified_segment(LinkedData::Models::Instance, [ont.acronym])
       page, size = page_params
-      reply LinkedData::InstanceLoader.get_instances_by_ontology(sub.id, page, size)
+
+      unmapped = (includes_param && includes_param.include?(:all))
+      page_data = LinkedData::Models::Instance.in(sub)
+                                              .include(LinkedData::Models::Instance.attributes)
+                                              .page(page,size).all
+
+      if unmapped && page_data.length > 0
+        LinkedData::Models::Instance.in(sub).models(page_data).include(:unmapped).all
+      end
+      reply page_data
     end
 
   end
