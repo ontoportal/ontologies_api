6f89433c1885e79decc75186d67723fa6dd9e6d1
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-05-23 18:24:17 +0200

    add the slice get endpoint

--- controllers/slices_controller.rb
+++ controllers/slices_controller.rb
@@ -6,5 +6,13 @@ class SlicesController < ApplicationController
       includes = LinkedData::Models::Slice.goo_attrs_to_load(includes_param)
       reply LinkedData::Models::Slice.where.include(includes).all
     end
+
+    get '/:slice_id' do
+      slice = LinkedData::Models::Slice.where(acronym: params["slice_id"]).first
+      error 404, "Slice #{params['slice_id']} not found" if slice.nil?
+      check_last_modified(slice)
+      slice.bring(*LinkedData::Models::Slice.goo_attrs_to_load(includes_param))
+      reply slice
+    end
   end
 end
