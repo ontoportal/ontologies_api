8adfb027a3e6e2954f35a1f6aaee609d75fb9c86
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2022-09-14 18:45:21 +0200

    use LinkedData::Mappings.create_mapping

--- controllers/mappings_controller.rb
+++ controllers/mappings_controller.rb
@@ -96,55 +96,12 @@ class MappingsController < ApplicationController
 
     # Create a new mapping
     post do
-      error(400, "Input does not contain classes") if !params[:classes]
-      if params[:classes].length > 2
-        error(400, "Input does not contain at least 2 terms")
+      begin
+        mapping = LinkedData::Mappings.create_mapping(mapping_hash: params, user_creator: find_user)
+        reply(201, mapping)
+      rescue StandardError => e
+        error(400, e.message)
       end
-      error(400, "Input does not contain mapping relation") if !params[:relation]
-      error(400, "Input does not contain user creator ID") if !params[:creator]
-      classes = []
-      params[:classes].each do |class_id,ontology_id|
-        o = ontology_id
-        o =  o.start_with?("http://") ? ontology_id :
-                                        ontology_uri_from_acronym(ontology_id)
-        o = LinkedData::Models::Ontology.find(RDF::URI.new(o))
-                                        .include(submissions:
-                                       [:submissionId, :submissionStatus]).first
-        if o.nil?
-          error(400, "Ontology with ID `#{ontology_id}` not found")
-        end
-        submission = o.latest_submission
-        if submission.nil?
-          error(400,
-     "Ontology with id #{ontology_id} does not have parsed valid submission")
-        end
-        submission.bring(ontology: [:acronym])
-        c = LinkedData::Models::Class.find(RDF::URI.new(class_id))
-                                    .in(submission)
-                                    .first
-        if c.nil?
-          error(400, "Class ID `#{id}` not found in `#{submission.id.to_s}`")
-        end
-        classes << c
-      end
-      user_id = params[:creator].start_with?("http://") ?
-                    params[:creator].split("/")[-1] : params[:creator]
-      user_creator = LinkedData::Models::User.find(user_id)
-                          .include(:username).first
-      if user_creator.nil?
-        error(400, "User with id `#{params[:creator]}` not found")
-      end
-      process = LinkedData::Models::MappingProcess.new(
-                    :creator => user_creator, :name => "REST Mapping")
-      process.relation = RDF::URI.new(params[:relation])
-      process.date = DateTime.now
-      process_fields = [:source,:source_name, :comment]
-      process_fields.each do |att|
-        process.send("#{att}=",params[att]) if params[att]
-      end
-      process.save
-      mapping = LinkedData::Mappings.create_rest_mapping(classes,process)
-      reply(201, mapping)
     end
 
     post '/load' do
