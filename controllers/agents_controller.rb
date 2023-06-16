0ddc5de47103d617b46ada5ea2a95bdea4ba3172
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-06-16 03:26:46 +0200

    handle agent indentifiers and affiliations attributes  save and update

--- controllers/agents_controller.rb
+++ controllers/agents_controller.rb
@@ -44,7 +44,12 @@ class AgentsController < ApplicationController
         if agent.nil?
           error 400, "Agent does not exist, please create using HTTP PUT before modifying"
         else
+          identifiers = params.delete "identifiers"
+          affiliations = params.delete "affiliations"
+          params.delete "id"
           populate_from_params(agent, params)
+          agent.identifiers = update_identifiers(identifiers)
+          agent.affiliations = update_affiliations(affiliations)
 
           if agent.valid?
             agent.save
@@ -64,6 +69,48 @@ class AgentsController < ApplicationController
 
       private
 
+      def update_identifiers(identifiers)
+        Array(identifiers).map do |i|
+          identifier = LinkedData::Models::AgentIdentifier.find(i["notation"]).first
+
+          if identifier
+            i.delete "id"
+            populate_from_params(identifier, i)
+          else
+            identifier = LinkedData::Models::AgentIdentifier.new(i)
+          end
+
+          if identifier.valid?
+            identifier.save
+          else
+            error 400, identifier.errors
+          end
+
+          identifier
+        end
+      end
+
+      def update_affiliations(affiliations)
+        Array(affiliations).map do |aff|
+          affiliations = LinkedData::Models::Agent.find(aff["id"])
+
+          if affiliations
+            aff.delete "id"
+            populate_from_params(affiliations, aff)
+          else
+            affiliations = LinkedData::Models::Agent.new(aff)
+          end
+
+          if affiliations.valid?
+            affiliations.save
+          else
+            error 400, affiliations.errors
+          end
+
+          affiliations
+        end
+      end
+
       def create_agent
         params ||= @params
         acronym = params["id"]
@@ -71,7 +118,12 @@ class AgentsController < ApplicationController
         agent = LinkedData::Models::Agent.find(acronym).include(LinkedData::Models::Agent.goo_attrs_to_load(includes_param)).first if acronym
 
         if agent.nil?
+          identifiers = params.delete "identifiers"
+          affiliations = params.delete "affiliations"
+          params.delete "id"
           agent = instance_from_params(LinkedData::Models::Agent, params)
+          agent.identifiers = update_identifiers(identifiers)
+          agent.affiliations = update_affiliations(affiliations)
         else
           error 400, "Agent exists, please use HTTP PATCH to update"
         end
