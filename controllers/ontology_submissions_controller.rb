070b1fb3cd1b50edc11dbbb3c8b8d9fd50e089cb
Syphax Bouazzouni <gs_bouazzouni@esi.dz>
2023-07-30 02:13:29 +0200

    bring the agent attributes on display all of the submissions endpoints

--- controllers/ontology_submissions_controller.rb
+++ controllers/ontology_submissions_controller.rb
@@ -31,9 +31,7 @@ class OntologySubmissionsController < ApplicationController
       check_access(ont)
       if includes_param.first == :all
         # When asking to display all metadata, we are using bring_remaining which is more performant than including all metadata (remove this when the query to get metadata will be fixed)
-        ont.bring(submissions: [:released, :creationDate, :status, :submissionId,
-                                {:contact=>[:name, :email], :ontology=>[:administeredBy, :acronym, :name, :summaryOnly, :ontologyType, :viewingRestriction, :acl, :group, :hasDomain, :views, :viewOf, :flat],
-                                 :submissionStatus=>[:code], :hasOntologyLanguage=>[:acronym]}, :submissionStatus], :metrics =>[:classes, :individuals, :properties])
+        ont.bring(submission_attributes_all)
 
         ont.submissions.each do |sub|
           sub.bring_remaining
--- helpers/submission_helper.rb
+++ helpers/submission_helper.rb
@@ -4,20 +4,28 @@ module Sinatra
   module Helpers
     module SubmissionHelper
 
+      def submission_attributes_all
+        out = [LinkedData::Models::OntologySubmission.embed_values_hash]
+        out << {:contact=>[:name, :email]}
+        out << {:ontology=>[:acronym, :name, :administeredBy, :group, :viewingRestriction, :doNotUpdate, :flat,
+                            :hasDomain, :summaryOnly, :acl, :viewOf, :ontologyType]}
+
+        out
+      end
+
       def retrieve_submissions(options)
         status = (options[:status] || "RDF").to_s.upcase
         status = "RDF" if status.eql?("READY")
         any = status.eql?("ANY")
         include_views = options[:also_include_views] || false
-        includes, page, size, order_by, _  =  settings_params(LinkedData::Models::OntologySubmission)
+        includes, page, size, order_by, _ = settings_params(LinkedData::Models::OntologySubmission)
         includes << :submissionStatus unless includes.include?(:submissionStatus)
-
 
         submissions_query = LinkedData::Models::OntologySubmission
         if any
           submissions_query = submissions_query.where
         else
-          submissions_query = submissions_query.where({submissionStatus: [ code: status]})
+          submissions_query = submissions_query.where({ submissionStatus: [code: status] })
         end
 
         submissions_query = apply_submission_filters(submissions_query)
@@ -26,18 +34,18 @@ module Sinatra
 
         # When asking to display all metadata, we are using bring_remaining on each submission. Slower but best way to retrieve all attrs
         if includes_param.first == :all
-          includes = [:submissionId, {:contact=>[:name, :email],
-                                      :ontology=>[:administeredBy, :acronym, :name, :summaryOnly, :ontologyType, :viewingRestriction, :acl,
-                                                                             :group, :hasDomain, :views, :viewOf, :flat, :notes, :reviews, :projects],
-                                      :submissionStatus=>[:code], :hasOntologyLanguage=>[:acronym], :metrics =>[:classes, :individuals, :properties]},
+          includes = [:submissionId, { :contact => [:name, :email],
+                                       :ontology => [:administeredBy, :acronym, :name, :summaryOnly, :ontologyType, :viewingRestriction, :acl,
+                                                     :group, :hasDomain, :views, :viewOf, :flat, :notes, :reviews, :projects],
+                                       :submissionStatus => [:code], :hasOntologyLanguage => [:acronym], :metrics => [:classes, :individuals, :properties] },
                       :submissionStatus]
         else
-          if includes.find{|v| v.is_a?(Hash) && v.keys.include?(:ontology)}
-           includes << {:ontology=>[:administeredBy, :acronym, :name, :viewingRestriction, :group, :hasDomain,:notes, :reviews, :projects,:acl, :viewOf]}
+          if includes.find { |v| v.is_a?(Hash) && v.keys.include?(:ontology) }
+            includes << { :ontology => [:administeredBy, :acronym, :name, :viewingRestriction, :group, :hasDomain, :notes, :reviews, :projects, :acl, :viewOf] }
           end
 
-          if includes.find{|v| v.is_a?(Hash) && v.keys.include?(:contact)}
-            includes << {:contact=>[:name, :email]}
+          if includes.find { |v| v.is_a?(Hash) && v.keys.include?(:contact) }
+            includes << { :contact => [:name, :email] }
           end
         end
 
