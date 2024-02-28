ded6a00d3518a92cfee5167d9500564492ba608a
mdorf <mdorf@stanford.edu>
2024-02-28 14:10:15 -0800

    fixed search tests

--- test/controllers/test_search_controller.rb
+++ test/controllers/test_search_controller.rb
@@ -3,62 +3,62 @@ require_relative '../test_case'
 class TestSearchController < TestCase
 
   def self.before_suite
-    #  count, acronyms, bro = LinkedData::SampleData::Ontology.create_ontologies_and_submissions({
-    #   process_submission: true,
-    #   acronym: "BROSEARCHTEST",
-    #   name: "BRO Search Test",
-    #   file_path: "./test/data/ontology_files/BRO_v3.2.owl",
-    #   ont_count: 1,
-    #   submission_count: 1,
-    #   ontology_type: "VALUE_SET_COLLECTION"
-    # })
-    #
-    # count, acronyms, mccl = LinkedData::SampleData::Ontology.create_ontologies_and_submissions({
-    #   process_submission: true,
-    #   acronym: "MCCLSEARCHTEST",
-    #   name: "MCCL Search Test",
-    #   file_path: "./test/data/ontology_files/CellLine_OWL_BioPortal_v1.0.owl",
-    #   ont_count: 1,
-    #   submission_count: 1
-    # })
-    #
-    # @@ontologies = bro.concat(mccl)
-    #
-    # @@test_user = LinkedData::Models::User.new(
-    #     username: "test_search_user",
-    #     email: "ncbo_search_user@example.org",
-    #     password: "test_user_password"
-    # )
-    # @@test_user.save
-    #
-    # # Create a test ROOT provisional class
-    # @@test_pc_root = LinkedData::Models::ProvisionalClass.new({
-    #   creator: @@test_user,
-    #   label: "Provisional Class - ROOT",
-    #   synonym: ["Test synonym for Prov Class ROOT", "Test syn ROOT provisional class"],
-    #   definition: ["Test definition for Prov Class ROOT"],
-    #   ontology: @@ontologies[0]
-    # })
-    # @@test_pc_root.save
-    #
-    # @@cls_uri = RDF::URI.new("http://bioontology.org/ontologies/ResearchArea.owl#Area_of_Research")
-    # # Create a test CHILD provisional class
-    # @@test_pc_child = LinkedData::Models::ProvisionalClass.new({
-    #   creator: @@test_user,
-    #   label: "Provisional Class - CHILD",
-    #   synonym: ["Test synonym for Prov Class CHILD", "Test syn CHILD provisional class"],
-    #   definition: ["Test definition for Prov Class CHILD"],
-    #   ontology: @@ontologies[0],
-    #   subclassOf: @@cls_uri
-    # })
-    # @@test_pc_child.save
+     count, acronyms, bro = LinkedData::SampleData::Ontology.create_ontologies_and_submissions({
+      process_submission: true,
+      acronym: "BROSEARCHTEST",
+      name: "BRO Search Test",
+      file_path: "./test/data/ontology_files/BRO_v3.2.owl",
+      ont_count: 1,
+      submission_count: 1,
+      ontology_type: "VALUE_SET_COLLECTION"
+    })
+
+    count, acronyms, mccl = LinkedData::SampleData::Ontology.create_ontologies_and_submissions({
+      process_submission: true,
+      acronym: "MCCLSEARCHTEST",
+      name: "MCCL Search Test",
+      file_path: "./test/data/ontology_files/CellLine_OWL_BioPortal_v1.0.owl",
+      ont_count: 1,
+      submission_count: 1
+    })
+
+    @@ontologies = bro.concat(mccl)
+
+    @@test_user = LinkedData::Models::User.new(
+        username: "test_search_user",
+        email: "ncbo_search_user@example.org",
+        password: "test_user_password"
+    )
+    @@test_user.save
+
+    # Create a test ROOT provisional class
+    @@test_pc_root = LinkedData::Models::ProvisionalClass.new({
+      creator: @@test_user,
+      label: "Provisional Class - ROOT",
+      synonym: ["Test synonym for Prov Class ROOT", "Test syn ROOT provisional class"],
+      definition: ["Test definition for Prov Class ROOT"],
+      ontology: @@ontologies[0]
+    })
+    @@test_pc_root.save
+
+    @@cls_uri = RDF::URI.new("http://bioontology.org/ontologies/ResearchArea.owl#Area_of_Research")
+    # Create a test CHILD provisional class
+    @@test_pc_child = LinkedData::Models::ProvisionalClass.new({
+      creator: @@test_user,
+      label: "Provisional Class - CHILD",
+      synonym: ["Test synonym for Prov Class CHILD", "Test syn CHILD provisional class"],
+      definition: ["Test definition for Prov Class CHILD"],
+      ontology: @@ontologies[0],
+      subclassOf: @@cls_uri
+    })
+    @@test_pc_child.save
   end
 
   def self.after_suite
-    # @@test_pc_root.delete
-    # @@test_pc_child.delete
+    @@test_pc_root.delete
+    @@test_pc_child.delete
     LinkedData::SampleData::Ontology.delete_ontologies_and_submissions
-    # @@test_user.delete
+    @@test_user.delete
     LinkedData::Models::Ontology.indexClear
     LinkedData::Models::Ontology.indexCommit
   end
