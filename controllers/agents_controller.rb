ab38c60a5d8a41c594a682789ef89dc01d5e7ed6
Bilel Kihal <61744974+Bilelkihal@users.noreply.github.com>
2024-07-23 15:22:46 +0200

    fix: remove duplicated agents endpoint ('/Agents') (#85)

--- controllers/agents_controller.rb
+++ controllers/agents_controller.rb
@@ -1,150 +1,148 @@
 class AgentsController < ApplicationController
 
-  %w[/agents /Agents].each do |namespace|
-    namespace namespace do
-      # Display all agents
-      get do
-        check_last_modified_collection(LinkedData::Models::Agent)
-        query = LinkedData::Models::Agent.where
-        query = apply_filters(LinkedData::Models::Agent, query)
-        query = query.include(LinkedData::Models::Agent.goo_attrs_to_load(includes_param))
-        if page?
-          page, size = page_params
-          agents = query.page(page, size).all
-        else
-          agents = query.to_a
-        end
-
-        if includes_param.include?(:all) || includes_param.include?(:usages)
-          LinkedData::Models::Agent.load_agents_usages(agents)
-        end
-
-        reply agents
+  namespace "/agents" do
+    get do
+      check_last_modified_collection(LinkedData::Models::Agent)
+      query = LinkedData::Models::Agent.where
+      query = apply_filters(LinkedData::Models::Agent, query)
+      query = query.include(LinkedData::Models::Agent.goo_attrs_to_load(includes_param))
+      if page?
+        page, size = page_params
+        agents = query.page(page, size).all
+      else
+        agents = query.to_a
       end
 
-      # Display a single agent
-      get '/:id' do
-        check_last_modified_collection(LinkedData::Models::Agent)
-        id = params["id"]
-        agent = LinkedData::Models::Agent.find(id).include(LinkedData::Models::Agent.goo_attrs_to_load(includes_param)).first
-        error 404, "Agent #{id} not found" if agent.nil?
-        reply 200, agent
+      if includes_param.include?(:all) || includes_param.include?(:usages)
+        LinkedData::Models::Agent.load_agents_usages(agents)
       end
 
-      # Create a agent with the given acronym
-      post do
-        reply 201, create_new_agent
-      end
-
-      # Create a agent with the given acronym
-      put '/:acronym' do
-        reply 201, create_new_agent
-      end
-
-      # Update an existing submission of a agent
-      patch '/:id' do
-        acronym = params["id"]
-        agent = LinkedData::Models::Agent.find(acronym).include(LinkedData::Models::Agent.attributes).first
-
-        if agent.nil?
-          error 400, "Agent does not exist, please create using HTTP PUT before modifying"
-        else
-          agent = update_agent(agent, params)
-
-          error 400, agent.errors unless agent.errors.empty?
-        end
-        halt 204
-      end
-
-      # Delete a agent
-      delete '/:id' do
-        agent = LinkedData::Models::Agent.find(params["id"]).first
-        agent.delete
-        halt 204
-      end
-
-      private
-
-      def update_identifiers(identifiers)
-        Array(identifiers).map do |i|
-          next nil if i.empty?
-
-          id =  i["id"] || LinkedData::Models::AgentIdentifier.generate_identifier(i['notation'], i['schemaAgency'])
-          identifier = LinkedData::Models::AgentIdentifier.find(RDF::URI.new(id)).first
-
-          if identifier
-            identifier.bring_remaining
-          else
-            identifier = LinkedData::Models::AgentIdentifier.new
-          end
-
-          i.delete "id"
-
-          next identifier if i.keys.size.zero?
-
-          populate_from_params(identifier, i)
-
-          if identifier.valid?
-            identifier.save
-          else
-            error 400, identifier.errors
-          end
-          identifier
-        end.compact
-      end
-
-      def update_affiliations(affiliations)
-        Array(affiliations).map do |aff|
-          affiliation =  aff["id"] ? LinkedData::Models::Agent.find(RDF::URI.new(aff["id"])).first : nil
-
-          if affiliation
-            affiliation.bring_remaining
-            affiliation.identifiers.each{|i| i.bring_remaining}
-          end
-
-          next affiliation if aff.keys.size.eql?(1) && aff["id"]
-
-          if affiliation
-            affiliation = update_agent(affiliation, aff)
-          else
-            affiliation = create_new_agent(aff["id"], aff)
-          end
-
-          error 400, affiliation.errors unless affiliation.errors.empty?
-
-          affiliation
-        end
-      end
-
-      def create_new_agent (id = @params['id'], params = @params)
-        agent = nil
-        agent = LinkedData::Models::Agent.find(id).include(LinkedData::Models::Agent.goo_attrs_to_load(includes_param)).first if id
-
-        if agent.nil?
-          agent = update_agent(LinkedData::Models::Agent.new, params)
-          error 400, agent.errors unless agent.errors.empty?
-
-          return agent
-        else
-          error 400, "Agent exists, please use HTTP PATCH to update"
-        end
-      end
-
-      def update_agent(agent, params)
-        return agent unless agent
-
-        identifiers = params.delete "identifiers"
-        affiliations = params.delete "affiliations"
-        params.delete "id"
-        populate_from_params(agent, params)
-        agent.identifiers = update_identifiers(identifiers)
-        agent.affiliations = update_affiliations(affiliations)
-
-        agent.save if agent.valid?
-        return agent
-      end
-
+      reply agents
     end
+
+    # Display a single agent
+    get '/:id' do
+      check_last_modified_collection(LinkedData::Models::Agent)
+      id = params["id"]
+      agent = LinkedData::Models::Agent.find(id).include(LinkedData::Models::Agent.goo_attrs_to_load(includes_param)).first
+      error 404, "Agent #{id} not found" if agent.nil?
+      reply 200, agent
+    end
+
+    # Create a agent with the given acronym
+    post do
+      reply 201, create_new_agent
+    end
+
+    # Create a agent with the given acronym
+    put '/:acronym' do
+      reply 201, create_new_agent
+    end
+
+    # Update an existing submission of a agent
+    patch '/:id' do
+      acronym = params["id"]
+      agent = LinkedData::Models::Agent.find(acronym).include(LinkedData::Models::Agent.attributes).first
+
+      if agent.nil?
+        error 400, "Agent does not exist, please create using HTTP PUT before modifying"
+      else
+        agent = update_agent(agent, params)
+
+        error 400, agent.errors unless agent.errors.empty?
+      end
+      halt 204
+    end
+
+    # Delete a agent
+    delete '/:id' do
+      agent = LinkedData::Models::Agent.find(params["id"]).first
+      agent.delete
+      halt 204
+    end
+
+    private
+
+    def update_identifiers(identifiers)
+      Array(identifiers).map do |i|
+        next nil if i.empty?
+
+        id =  i["id"] || LinkedData::Models::AgentIdentifier.generate_identifier(i['notation'], i['schemaAgency'])
+        identifier = LinkedData::Models::AgentIdentifier.find(RDF::URI.new(id)).first
+
+        if identifier
+          identifier.bring_remaining
+        else
+          identifier = LinkedData::Models::AgentIdentifier.new
+        end
+
+        i.delete "id"
+
+        next identifier if i.keys.size.zero?
+
+        populate_from_params(identifier, i)
+
+        if identifier.valid?
+          identifier.save
+        else
+          error 400, identifier.errors
+        end
+        identifier
+      end.compact
+    end
+
+    def update_affiliations(affiliations)
+      Array(affiliations).map do |aff|
+        affiliation =  aff["id"] ? LinkedData::Models::Agent.find(RDF::URI.new(aff["id"])).first : nil
+
+        if affiliation
+          affiliation.bring_remaining
+          affiliation.identifiers.each{|i| i.bring_remaining}
+        end
+
+        next affiliation if aff.keys.size.eql?(1) && aff["id"]
+
+        if affiliation
+          affiliation = update_agent(affiliation, aff)
+        else
+          affiliation = create_new_agent(aff["id"], aff)
+        end
+
+        error 400, affiliation.errors unless affiliation.errors.empty?
+
+        affiliation
+      end
+    end
+
+    def create_new_agent (id = @params['id'], params = @params)
+      agent = nil
+      agent = LinkedData::Models::Agent.find(id).include(LinkedData::Models::Agent.goo_attrs_to_load(includes_param)).first if id
+
+      if agent.nil?
+        agent = update_agent(LinkedData::Models::Agent.new, params)
+        error 400, agent.errors unless agent.errors.empty?
+
+        return agent
+      else
+        error 400, "Agent exists, please use HTTP PATCH to update"
+      end
+    end
+
+    def update_agent(agent, params)
+      return agent unless agent
+
+      identifiers = params.delete "identifiers"
+      affiliations = params.delete "affiliations"
+      params.delete "id"
+      populate_from_params(agent, params)
+      agent.identifiers = update_identifiers(identifiers)
+      agent.affiliations = update_affiliations(affiliations)
+
+      agent.save if agent.valid?
+      return agent
+    end
+
   end
+
 
 end
